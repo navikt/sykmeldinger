6ca6198d4e413c22691ce4bf139c5c4ee4beb095
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-11-06 12:56:49 +0100

    endrer henting av mock sykmelding

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -3,16 +3,17 @@ import Spinner from 'nav-frontend-spinner';
 import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 import useFetch, { isNotStarted, FetchState, hasData, isAnyNotStartedOrPending, hasAnyFailed } from '../hooks/useFetch';
 import useAppStore from '../store/useAppStore';
+import { Sykmelding } from '../types/sykmeldingTypes';
 
 const DataFetcher = (props: { children: any }) => {
     const { setSykmeldinger, setNaermesteLedere } = useAppStore();
-    const sykmeldingerFetcher = useFetch<any[]>();
+    const sykmeldingerFetcher = useFetch<Sykmelding[]>();
 
     useEffect(() => {
         if (isNotStarted(sykmeldingerFetcher)) {
             sykmeldingerFetcher.fetch('/syforest/sykmeldinger', undefined, (fetchState: FetchState<any[]>) => {
                 if (hasData(fetchState)) {
-                    setSykmeldinger(fetchState.data);
+                    setSykmeldinger(fetchState.data.map(sykmelding => new Sykmelding(sykmelding.receivedSykmelding.sykmelding)));
                 }
             });
         }
