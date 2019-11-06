ab5ae71b77f5fba856d549d3e556f7782e1c239b
Jonas Henie <jonas.henie@nav.no>
2019-11-06 15:23:14 +0100

    Legg til switcher på sykmeldingsside basert på sykmeldingstatus

--- src/pages/SykmeldingSide.tsx
+++ src/pages/SykmeldingSide.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-import Sykmelding from '../components/sykmelding/sykmelding';
 import Brodsmuler, { Brodsmule } from '../components/brodsmuler/brodsmuler';
+import useAppStore from '../store/useAppStore';
+import NySykmelding from '../components/sykmelding/NySykmelding';
+import { Status } from '../types/sykmeldingDataTypes';
+import AvvistSykmelding from '../components/sykmelding/AvvistSykmelding';
+import AvbruttSykmelding from '../components/sykmelding/AvbruttSykmelding';
 
 const brodsmuler: Brodsmule[] = [
     {
@@ -21,12 +25,37 @@ const brodsmuler: Brodsmule[] = [
     },
 ];
 
-const SykmeldingSide: React.FC = () => {
+const SykmeldingSide: React.FC = props => {
+    const { sykmelding, sykmeldingType } = useAppStore();
+
+    if (!sykmeldingType) {
+        // TODO: Error-melding, ingen sykmelding funnet
+        return null;
+    }
+
+    const SykmeldingComponent = (() => {
+        switch (sykmeldingType) {
+            case Status.NY:
+                return <NySykmelding sykmeldingtype={'NY'} />;
+            case Status.AVBRUTT:
+                return <AvbruttSykmelding sykmeldingtype={'NY'} />;
+            case Status.AVVIST:
+                return <AvvistSykmelding sykmeldingtype={'NY'} />;
+            default:
+                return null;
+        }
+    })();
+
+    if (!SykmeldingComponent) {
+        // TODO: Error-melding, ingen gyldig sykemeldingstype definert
+        return null;
+    }
+
     return (
         <div className="limit">
             <Brodsmuler brodsmuler={brodsmuler} />
             <p>Sykmelding side</p>
-            <Sykmelding sykmeldingtype={'NY'} />
+            {SykmeldingComponent}
         </div>
     );
 };
