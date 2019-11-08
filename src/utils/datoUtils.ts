500b958b06c347513e29d12f9cea512fdc3e0917
Jonas Henie <jonas.henie@nav.no>
2019-11-08 12:12:54 +0100

    La til datoUtils for periodevisning

---
+++ src/utils/datoUtils.ts
@@ -0,0 +1,51 @@
+const maaneder = [
+    'januar',
+    'februar',
+    'mars',
+    'april',
+    'mai',
+    'juni',
+    'juli',
+    'august',
+    'september',
+    'oktober',
+    'november',
+    'desember',
+];
+const SKILLETEGN_PERIODE = '–';
+
+export const tilLesbarPeriodeMedArstall = (fomArg, tomArg) => {
+    const fom = new Date(fomArg);
+    const tom = new Date(tomArg);
+    const erSammeAar = fom.getFullYear() === tom.getFullYear();
+    const erSammeMaaned = fom.getMonth() === tom.getMonth();
+    return erSammeAar && erSammeMaaned
+        ? `${fom.getDate()}. ${SKILLETEGN_PERIODE} ${tilLesbarDatoMedArstall(tom)}`
+        : erSammeAar
+        ? `${tilLesbarDatoUtenAarstall(fom)} ${SKILLETEGN_PERIODE} ${tilLesbarDatoMedArstall(tom)}`
+        : `${tilLesbarDatoMedArstall(fom)} ${SKILLETEGN_PERIODE} ${tilLesbarDatoMedArstall(tom)}`;
+};
+
+export const tilLesbarDatoMedArstall = datoArg => {
+    return datoArg ? `${tilLesbarDatoUtenAarstall(new Date(datoArg))} ${new Date(datoArg).getFullYear()}` : null;
+};
+
+export const tilLesbarPeriodeUtenArstall = (fomArg, tomArg) => {
+    const fom = new Date(fomArg);
+    const tom = new Date(tomArg);
+    const erSammeMaaned = fom.getMonth() === tom.getMonth();
+    return erSammeMaaned
+        ? `${fom.getDate()}. ${SKILLETEGN_PERIODE} ${tilLesbarDatoUtenAarstall(tom)}`
+        : `${tilLesbarDatoUtenAarstall(fom)} ${SKILLETEGN_PERIODE} ${tilLesbarDatoUtenAarstall(tom)}`;
+};
+
+export const tilLesbarDatoUtenAarstall = datoArg => {
+    if (datoArg) {
+        const dato = new Date(datoArg);
+        const dag = dato.getDate();
+        const manedIndex = dato.getMonth();
+        const maned = maaneder[manedIndex];
+        return `${dag}. ${maned}`;
+    }
+    return null;
+};
