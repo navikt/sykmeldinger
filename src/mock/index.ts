f8d2831db768da17d001242358818825823b50ac
Jonas Henie <jonas.henie@nav.no>
2019-11-18 15:44:43 +0100

    Fiks merge-feil

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,5 +1,5 @@
 import FetchMock, { MiddlewareUtils } from 'yet-another-fetch-mock';
-import { nySykmeldingMock, avvistSykmeldingMock } from './data/sykmeldingMock';
+import { nySykmeldingMock } from './data/sykmeldingMock';
 import naermesteLedereMock from './data/narmesteLedereMock';
 
 const mock = FetchMock.configure({
@@ -8,5 +8,4 @@ const mock = FetchMock.configure({
 });
 
 mock.get('/syforest/sykmelding', nySykmeldingMock);
-mock.get('/syforest/sykmelding/avvist', avvistSykmeldingMock);
 mock.get('/syforest/naermesteledere', naermesteLedereMock);
