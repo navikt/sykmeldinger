31c357b0f6974ec6cf6f537adea60cb52d3f64aa
Jonas Henie <jonas.henie@nav.no>
2019-11-21 15:41:55 +0100

    Enums i samme fil

--- src/components/sykmelding/avvistsykmelding/components/Begrunnelser.tsx
+++ src/components/sykmelding/avvistsykmelding/components/Begrunnelser.tsx
@@ -3,14 +3,15 @@ import { Element } from 'nav-frontend-typografi';
 
 import tekster from '../avvistsykmelding-tekster';
 import { Sykmelding } from '../../../../types/sykmeldingTypes';
-import {
-    PASIENT_ELDRE_ENN_70,
-    UGYLDIG_REGELSETTVERSJON,
-    BEHANDLER_IKKE_GYLDIG_I_HPR,
-    BEHANDLER_MANGLER_AUTORISASJON_I_HPR,
-    BEHANDLER_IKKE_LE_KI_MT_TL_FT_I_HPR,
-    BEHANDLER_SUSPENDERT,
-} from '../../../../enums/avvisningsregelnavn';
+
+export const PASIENT_ELDRE_ENN_70 = 'PASIENT_ELDRE_ENN_70';
+export const UKJENT_DIAGNOSEKODETYPE = 'UKJENT_DIAGNOSEKODETYPE';
+export const UGYLDIG_KODEVERK_FOR_BIDIAGNOSE = 'UGYLDIG_KODEVERK_FOR_BIDIAGNOSE';
+export const UGYLDIG_REGELSETTVERSJON = 'UGYLDIG_REGELSETTVERSJON';
+export const BEHANDLER_IKKE_GYLDIG_I_HPR = 'BEHANDLER_IKKE_GYLDIG_I_HPR';
+export const BEHANDLER_MANGLER_AUTORISASJON_I_HPR = 'BEHANDLER_MANGLER_AUTORISASJON_I_HPR';
+export const BEHANDLER_IKKE_LE_KI_MT_TL_FT_I_HPR = 'BEHANDLER_IKKE_LE_KI_MT_TL_FT_I_HPR';
+export const BEHANDLER_SUSPENDERT = 'BEHANDLER_SUSPENDERT';
 
 interface BegrunnelserProps {
     sykmelding: Sykmelding;
