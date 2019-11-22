ba5387e6c1f3c6fb63a418c637b93230bbe3c610
Jonas Henie <jonas.henie@nav.no>
2019-11-22 15:46:44 +0100

    Fiks statuspanel for bekreftede sykmeldinger

--- src/components/statuspanel/BekreftetStatuspanel.tsx
+++ src/components/statuspanel/BekreftetStatuspanel.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Sykmelding } from '../../types/sykmeldingTypes';
 import EtikettMedTekst from '../infopanel/layout/EtikettMedTekst';
 import { tilLesbarDatoMedArstall } from '../../utils/datoUtils';
-import { Knapp } from 'nav-frontend-knapper';
+import { Panel } from 'nav-frontend-paneler';
 
 import './statuspanel.less';
+import Margin from '../infopanel/layout/Margin';
 
 interface BekreftetStatuspanelProps {
     sykmelding: Sykmelding;
@@ -20,13 +21,19 @@ const BekreftetStatuspanel = ({ sykmelding }: BekreftetStatuspanelProps) => {
     // dato bekreftet
 
     return (
-        <div className="statuspanel">
-            <div className="statuspanel-rad">
-                <EtikettMedTekst tittel="Status" tekst="Bekreftet av deg" />
-                <EtikettMedTekst tittel="Dato bekreftet" tekst={tilLesbarDatoMedArstall(new Date())} />
-                <EtikettMedTekst tittel="Jeg er sykmeldt fra" tekst="TODO: Sykmeldt fra" />
-            </div>
-        </div>
+        <Margin>
+            <Panel border className="statuspanel">
+                <div className="statuspanel__element">
+                    <EtikettMedTekst tittel="Status" tekst="Bekreftet av deg" />
+                </div>
+                <div className="statuspanel__element">
+                    <EtikettMedTekst tittel="Dato bekreftet" tekst={tilLesbarDatoMedArstall(new Date())} />
+                </div>
+                <div className="statuspanel__element">
+                    <EtikettMedTekst tittel="Jeg er sykmeldt fra" tekst="TODO: Sykmeldt fra" />
+                </div>
+            </Panel>
+        </Margin>
     );
 };
 
