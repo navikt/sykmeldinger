80eed66753fd0e82a838c9f8bfe31afc6e6d261c
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-02-18 13:16:35 +0100

    legger til default sykmeldinger

--- src/mock/DemoWrapper.tsx
+++ src/mock/DemoWrapper.tsx
@@ -7,20 +7,31 @@ Denne komponenten vises kun i dev-modus, og lar deg skifte mellom ulike sykmeldi
 */
 
 const DemoWrapper = () => {
+    const demoSykmeldinger = new Map<string, string>([
+        ['Ny sykmelding', '/fravaer/steg-1-variasjon-1-a/ny-sykmelding-variasjon-1-sykmelding'],
+        ['Sendt sykmelding', '/fravaer/steg-1-variasjon-1-b/ferdig-sykmelding-variasjon-1-sykmelding-a'],
+    ]);
+
     return (
         <>
             <select
-                style={{ position: 'absolute', right: 0, zIndex: 1, width: 20, background: 'white' }}
+                style={{ position: 'absolute', right: 0, zIndex: 1, background: 'white' }}
                 name="sykmeldinger"
                 id="sykmelding-select"
                 onChange={event => {
                     console.log(event.target.value);
+                    window.location.href = event.target.value;
                 }}
-                value="init"
+                defaultValue="init"
             >
                 <option key="init" value="">
-                    -- Bytt sykmelding --
+                    -- Velg sykmelding --
                 </option>
+                {Array.from(demoSykmeldinger.entries()).map(([key, value], index) => (
+                    <option key={index} value={value}>
+                        {key}
+                    </option>
+                ))}
             </select>
             <button
                 style={{ position: 'absolute' }}
