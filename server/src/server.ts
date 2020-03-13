dbc243981c9a0710a0b07e3005303b408fc6d1ac
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-03-13 14:07:05 +0100

    audit fix

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import path from 'path';
+
+const server = express();
+const PORT = process.env['PORT'] || 3000;
+
+try {
+    server.use(express.static(path.join(__dirname, '../../client/build')));
+    server.listen(PORT, () => {});
+} catch (error) {
+    console.error('Error during startup', error);
+}
