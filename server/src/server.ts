9e14a697e344b6381e3ec8963040c492049593c5
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-03-25 15:26:39 +0100

    endepunkter for is_alive is_ready

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,13 @@ const PORT = process.env['PORT'] || 3000;
 
 try {
     server.use(express.static(path.join(__dirname, '../../client/build')));
-    server.listen(PORT, () => {});
+
+    server.get('/is_alive', (_req, res) => res.status(200).send('alive'));
+    server.get('/is_ready', (_req, res) => res.status(200).send('ready'));
+
+    server.listen(PORT, () => {
+        console.log(`Server running on port: ${PORT})`);
+    });
 } catch (error) {
     console.error('Error during startup', error);
 }
