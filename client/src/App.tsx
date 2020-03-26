e1a432861227512dd3b1b78a8813658fcc1cbdc7
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-03-26 15:33:52 +0100

    endrer routing i react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,12 +7,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SykmeldingSide from './pages/SykmeldingSide';
 import useAppStore from './data/useAppStore';
 
+const Testcomponent = ({ text = 'hello world' }: any) => {
+    return <p>{text}</p>;
+};
+
 const App = () => {
     return (
         <useAppStore.Provider>
             <BrowserRouter>
                 <Switch>
                     <Route path={`${process.env.REACT_APP_SYKMELDING_ROOT}/:sykmeldingId`} component={SykmeldingSide} />
+                    <Route path="/:sykmeldingId" component={SykmeldingSide} />
+                    <Route path="/" component={Testcomponent} />
                 </Switch>
             </BrowserRouter>
         </useAppStore.Provider>
