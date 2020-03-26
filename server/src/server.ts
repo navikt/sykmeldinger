feafce23a3ba40d888fd2144ece011b5aa3a14c0
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-03-26 14:54:03 +0100

    legger til cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import path from 'path';
+import cors, { CorsOptions } from 'cors';
 
 const server = express();
 const PORT = process.env['PORT'] || 3000;
 
+const corsOptions: CorsOptions = {
+    origin: '*'
+}
+
 try {
-    
+
+    server.use(cors(corsOptions));
+
     server.get('/is_alive', (_req, res) => res.status(200).send('alive'));
     server.get('/is_ready', (_req, res) => res.status(200).send('ready'));
     
