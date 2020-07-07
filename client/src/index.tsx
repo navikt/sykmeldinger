53241c32699dab8f41b82beaed74b2b703138c20
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-07-07 16:10:55 +0200

    Removes unused components and classes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,9 @@
 import 'dayjs/locale/nb';
-
-import './index.less';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as dayjs from 'dayjs';
+import dayjs from 'dayjs';
 
 import withMenu from './mock/decorator/decorator-header-withmenu';
 import megamenu from './mock/decorator/decorator-megamenu';
@@ -15,43 +12,31 @@ import scripts from './mock/decorator/decorator-scripts';
 import skiplinks from './mock/decorator/decorator-skiplinks';
 import styles from './mock/decorator/decorator-styles';
 
-import env from './utils/environment';
-
 dayjs.locale('nb');
 
-const init = () => {
-    if (process.env.NODE_ENV === 'development') {
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_HEADING}}}', withMenu);
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_FOOTER}}}', footer);
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_STYLES}}}', styles);
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SCRIPTS}}}', scripts);
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SKIPLINKS}}}', skiplinks);
-        document.body.innerHTML = document.body.innerHTML.replace('{{{MEGAMENU_RESOURCES}}}', megamenu);
+if (process.env.NODE_ENV === 'development') {
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_HEADING}}}', withMenu);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_FOOTER}}}', footer);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_STYLES}}}', styles);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SCRIPTS}}}', scripts);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SKIPLINKS}}}', skiplinks);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{MEGAMENU_RESOURCES}}}', megamenu);
 
-        // Execute client.js
-        var script = document.createElement('script');
-        script.src = 'https://www.nav.no/dekoratoren/client.js';
-        document.body.appendChild(script);
-    }
+    // Execute client.js
+    var script = document.createElement('script');
+    script.src = 'https://www.nav.no/dekoratoren/client.js';
+    document.body.appendChild(script);
+}
 
-    const REDIRECT_ETTER_LOGIN = window.localStorage.getItem('REDIRECT_ETTER_LOGIN');
-    if (REDIRECT_ETTER_LOGIN) {
-        // Fjerner slik at den ikke skal gå i loop
-        window.localStorage.removeItem('REDIRECT_ETTER_LOGIN');
-        window.location.href = REDIRECT_ETTER_LOGIN;
-    }
+if (process.env.NODE_ENV === 'development') {
+    require('./mock/mock');
+}
 
-    if (env.isDevelopment || env.isRunningOnHeroku) {
-        require('./mock/mock');
-    }
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('app'),
+);
 
-    ReactDOM.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>,
-        document.getElementById('app'),
-    );
-
-    serviceWorker.unregister();
-};
-init();
+serviceWorker.unregister();
