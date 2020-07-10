a509f9ecf9affa5eb81c43a6cb12e3a9d4999b71
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-07-10 10:46:20 +0200

    Implements sykmeldingerpage with lenkepanels

---
+++ client/src/pages/sykmeldinger/LenkepanelContainer.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Sykmelding } from '../../types/sykmelding';
+import { Undertittel } from 'nav-frontend-typografi';
+import { Select } from 'nav-frontend-skjema';
+import './LenkepanelContainer.less';
+import Lenkepanel from './Lenkepanel/Lenkepanel';
+import { sortSykmeldingerNewestFirst, sortSykmeldingerArbeidsgiver } from '../../utils/sorterSykemeldingUtils';
+
+export enum SortBy {
+    DATE = 'DATE',
+    ARBEIDSGIVER = 'ARBEIDSGIVER',
+}
+
+interface LenkepanelContainerProps {
+    title: string;
+    sykmeldinger: Sykmelding[];
+    showSortBy?: boolean;
+}
+
+const LenkepanelContainer = ({ title, sykmeldinger, showSortBy = false }: LenkepanelContainerProps) => {
+    const [sortBy, setSortBy] = useState(SortBy.DATE); // Sort by date as default
+    const [sykmeldingerSorted, setSykmeldingerSorted] = useState<Sykmelding[]>(
+        sortSykmeldingerNewestFirst(sykmeldinger),
+    );
+
+    const handleSortChange = (sortBy: SortBy): void => {
+        setSortBy(sortBy);
+        switch (sortBy) {
+            case 'DATE':
+                setSykmeldingerSorted(sortSykmeldingerNewestFirst(sykmeldinger));
+                break;
+            case 'ARBEIDSGIVER':
+                setSykmeldingerSorted(sortSykmeldingerArbeidsgiver(sykmeldinger));
+                break;
+        }
+    };
+
+    return (
+        <div className="lenkepanel-container">
+            <header className="lenkepanel-container__header">
+                <Undertittel>{title}</Undertittel>
+                {showSortBy ? (
+                    <Select
+                        value={sortBy}
+                        label="Sorter etter"
+                        onChange={(event) => handleSortChange(event.target.value as SortBy)}
+                    >
+                        <option value={SortBy.DATE}>Dato</option>
+                        <option value={SortBy.ARBEIDSGIVER}>Arbeidsgiver</option>
+                    </Select>
+                ) : null}
+            </header>
+            <ol className="lenkepanel-container__sykmeldinger">
+                {sykmeldingerSorted.map((sykmelding, index) => (
+                    <li key={index} className="lenkepanel-container__sykmelding">
+                        <Lenkepanel
+                            sykmeldingId={sykmelding.id}
+                            sykmeldingsstatus={
+                                sykmelding.behandlingsutfall.status === 'INVALID'
+                                    ? sykmelding.behandlingsutfall.status
+                                    : sykmelding.sykmeldingStatus.statusEvent
+                            }
+                            sykmeldingsperioder={sykmelding.sykmeldingsperioder}
+                            arbeidsgiverNavn={sykmelding.sykmeldingStatus.arbeidsgiver?.orgNavn}
+                        />
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
+};
+
+export default LenkepanelContainer;
