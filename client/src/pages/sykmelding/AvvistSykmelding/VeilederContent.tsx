a1ba437056e9eeedc7f1619607e7b91d01269dd0
Sebastian <sebastian.knudsen@nav.no>
2020-07-11 14:31:47 +0200

    implements veileder (#74)
    * implements veileder
    
    * fix avvist sykmelding veileder

--- client/src/pages/sykmelding/AvvistSykmelding/VeilederContent.tsx
+++ client/src/pages/sykmelding/AvvistSykmelding/VeilederContent.tsx
@@ -1,37 +1,20 @@
 import './VeilederContent.less';
 
 import React from 'react';
-import { Systemtittel, Element, Normaltekst } from 'nav-frontend-typografi';
+import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
 
-import Begrunnelser from './Begrunnelser';
 import { Behandler, Sykmelding } from '../../../types/sykmelding';
 
 const createLegenavn = (behandler: Behandler) => {
     const { fornavn, mellomnavn, etternavn } = behandler;
-
     let navn = fornavn;
-
     if (mellomnavn) {
         navn += ` ${mellomnavn}`;
     }
-
     if (etternavn) {
         navn += ` ${etternavn}`;
     }
-
     return navn;
-};
-
-const getIntro = (sykmelding: Sykmelding) => {
-    const legenavn = createLegenavn(sykmelding.behandler);
-    return `Du trenger en ny sykmelding fordi det er gjort en feil i utfyllingen. Vi har gitt beskjed til ${legenavn} om hva som er feil, og at du må få en ny sykmelding.`;
-};
-
-const getReason = (sykmelding: Sykmelding) => {
-    // TODO: Is the reason collected directly from the sykmelding object?
-    const reason = 'Første sykmelding er tilbakedatert mer enn det som er tillatt.';
-
-    return reason;
 };
 
 interface VeilederContentProps {
@@ -39,33 +22,33 @@ interface VeilederContentProps {
 }
 
 const VeilederContent = ({ sykmelding }: VeilederContentProps) => {
-    const intro = getIntro(sykmelding);
     const legenavn = createLegenavn(sykmelding.behandler);
-    const reason = getReason(sykmelding);
 
     return (
         <>
-            <Systemtittel className="veiledercontent__title">Sykmeldingen kan dessverre ikke brukes</Systemtittel>
-            <hr className="veiledercontent__underline" />
-            <>
-                <Normaltekst>Beklager at vi må bry deg mens du er syk.</Normaltekst>
-                <br />
-                <Normaltekst>{intro}</Normaltekst>
-                <br />
-                <Normaltekst>
-                    Når du har fått ny sykmelding fra {legenavn}, får du en ny beskjed fra oss om å logge deg inn på
-                    nav.no slik at du kan sende inn sykmeldingen. Går det mange dager, bør du kontakte {legenavn} som
-                    skal skrive den nye sykmeldingen.
-                </Normaltekst>
-            </>
-
+            <Undertittel className="veiledercontent__title">Sykmeldingen kan dessverre ikke brukes</Undertittel>
+            <hr aria-hidden className="veiledercontent__underline" />
+            <Normaltekst>Beklager at vi må bry deg mens du er syk.</Normaltekst>
             <br />
-
-            <>
-                <Element>Grunnen til at sykmeldingen er avvist:</Element>
-                <Normaltekst>{reason}</Normaltekst>
-            </>
-            <Begrunnelser sykmelding={sykmelding} />
+            <Normaltekst>
+                Du trenger en ny sykmelding fordi det er gjort en feil i utfyllingen. Vi har gitt beskjed til {legenavn}{' '}
+                om hva som er feil, og at du må få en ny sykmelding.
+            </Normaltekst>
+            <br />
+            <Normaltekst>
+                Når du har fått ny sykmelding fra {legenavn}, får du en ny beskjed fra oss om å logge deg inn på nav.no
+                slik at du kan sende inn sykmeldingen. Går det mange dager, bør du kontakte {legenavn} som skal skrive
+                den nye sykmeldingen.
+            </Normaltekst>
+            <br />
+            <Element>Grunnen til at sykmeldingen er avvist:</Element>
+            <ul>
+                {sykmelding.behandlingsutfall.ruleHits.map((ruleHit) => (
+                    <li>
+                        <Normaltekst>{ruleHit.messageForUser}</Normaltekst>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 };
