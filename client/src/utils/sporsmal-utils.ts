111f434107e2c8caf9fe30f53833b29a6f102b40
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-07-15 21:27:29 +0200

    remove old form files

--- client/src/utils/sporsmal-utils.ts
+++
@@ -1,8 +0,0 @@
-import { Sykmelding } from '../types/sykmelding';
-
-export const skalViseFrilansersporsmal = (sykmelding: Sykmelding, sykmeldingUtenforVentetid: boolean): boolean => {
-    // const mulighetForArbeid = sykmelding.perioder.find(periode => !!periode.aktivitetIkkeMulig);
-
-    // TODO: Rydd opp i (tilsynelatende) shady logikk fra sykefravear
-    return !sykmeldingUtenforVentetid;
-};
