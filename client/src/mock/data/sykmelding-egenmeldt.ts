75309e14b26891712e12537270b7e9a0d955d008
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-07-29 15:45:50 +0200

    Egenmeldt sykmelding component and mock

---
+++ client/src/mock/data/sykmelding-egenmeldt.ts
@@ -0,0 +1,85 @@
+export const sykmeldingEgenmeldt = {
+    id: 'EGENMELDT',
+    mottattTidspunkt: '01-10-2020',
+    behandlingsutfall: {
+        status: 'OK',
+        ruleHits: [],
+    },
+    arbeidsgiver: {
+        navn: 'Navn Navnesen',
+        stillingsprosent: 100,
+    },
+    sykmeldingsperioder: [
+        {
+            fom: '02-10-2020',
+            tom: '02-11-2020',
+            behandlingsdager: 2,
+            type: 'BEHANDLINGSDAGER',
+        },
+    ],
+    sykmeldingStatus: {
+        timestamp: '01-01-2020',
+        statusEvent: 'BEKREFTET',
+    },
+    medisinskVurdering: {
+        hovedDiagnose: {
+            system: '2.16.578.1.12.4.1.1.7170',
+            kode: 'K24',
+            tekst: 'Vondt i magen',
+        },
+        biDiagnoser: [
+            {
+                system: '2.16.578.1.12.4.1.1.7170',
+                kode: '-57',
+                tekst: 'Vondt i magen',
+            },
+            {
+                system: '2.16.578.1.12.4.1.1.7170',
+                kode: '-59',
+                tekst: 'Sykdom0',
+            },
+        ],
+        svangerskap: true,
+        yrkesskade: true,
+        yrkesskadeDato: '2018-10-18',
+        annenFraversArsak: {
+            beskrivelse:
+                'word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word',
+            grunn: ['NODVENDIG_KONTROLLUNDENRSOKELSE', 'ABORT'],
+        },
+    },
+    skjermesForPasient: false,
+    utdypendeOpplysninger: {
+        '6.2': {
+            '6.2.1': {
+                sporsmal: 'Dette er et spørsmål',
+                svar: 'Dette er et svar',
+                restriksjoner: [],
+            },
+        },
+    },
+    kontaktMedPasient: {},
+    behandletTidspunkt: '01-01-2020',
+    behandler: {
+        fornavn: 'Fornavn',
+        mellomnavn: null,
+        etternavn: 'Etternavn',
+        aktoerId: '1234',
+        fnr: '99999999999',
+        hpr: '321',
+        her: '123',
+        adresse: {
+            gate: 'Gateveien 4',
+            postnummer: 1001,
+            kommune: 'Oslo',
+            postboks: '1212 Gateveien',
+            land: 'NO',
+        },
+        tlf: '900 00 000',
+    },
+    syketilfelleStartDato: '2018-10-10',
+    navnFastlege: 'Doktor Legesen',
+    egenmeldt: true,
+    papirsykmelding: false,
+    harRedusertArbeidsgiverperiode: false,
+};
