35a21622ed9d552e50368980f3e4ed18c3c9032b
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-08-05 11:19:51 +0200

    implements getDecorator

---
+++ server/src/decorator.ts
@@ -0,0 +1,42 @@
+import { JSDOM } from 'jsdom';
+import request from 'request';
+import NodeCache from 'node-cache';
+
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
+
+// Refresh cache every hour
+const cache = new NodeCache({
+    stdTTL: SECONDS_PER_HOUR,
+    checkperiod: SECONDS_PER_MINUTE,
+});
+
+const getDecorator = () =>
+    new Promise((resolve, reject) => {
+        const decorator = cache.get('main-cache');
+        if (decorator) {
+            resolve(decorator);
+        } else {
+            request('https://nav.no/dekoratoren', (error, response, body) => {
+                if (!error && response.statusCode >= 200 && response.statusCode < 400) {
+                    const { document } = new JSDOM(body).window;
+                    const prop = 'innerHTML';
+                    const data = {
+                        NAV_SKIPLINKS: document.getElementById('skiplinks')![prop],
+                        NAV_SCRIPTS: document.getElementById('scripts')![prop],
+                        NAV_STYLES: document.getElementById('styles')![prop],
+                        NAV_HEADING: document.getElementById('header-withmenu')![prop],
+                        NAV_FOOTER: document.getElementById('footer-withmenu')![prop],
+                        MEGAMENU_RESOURCES: document.getElementById('megamenu-resources')![prop],
+                    };
+                    cache.set('main-cache', data);
+                    console.log(`Creating cache`);
+                    resolve(data);
+                } else {
+                    reject(new Error(error));
+                }
+            });
+        }
+    });
+
+export default getDecorator;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,20 @@
+require('dotenv').config();
 import express from 'express';
 import path from 'path';
 import cors, { CorsOptions } from 'cors';
 import morganBody from 'morgan-body';
+import mustacheExpress from 'mustache-express';
+import getDecorator from './decorator';
 
 const server = express();
 const PORT = process.env['PORT'] || 3000;
+const BUILD_PATH = path.join(__dirname, '../../client/build');
+
+server.set('views', BUILD_PATH);
+server.set('view engine', 'mustache');
+server.engine('html', mustacheExpress());
+
+console.log(process.env['DECORATOR_URL']);
 
 const corsOptions: CorsOptions = {
     origin: '*',
@@ -12,13 +22,27 @@ const corsOptions: CorsOptions = {
 
 try {
     morganBody(server);
-    server.use(express.static(path.join(__dirname, '../../client/build'), { etag: false })); // etag for turning off caching. not sure if this is the best way to deal with caching
     server.use(express.json());
     server.use(express.urlencoded({ extended: true }));
     server.use(cors(corsOptions));
 
+    // nais urls
     server.get('/is_alive', (_req, res) => res.status(200).send('alive'));
     server.get('/is_ready', (_req, res) => res.status(200).send('ready'));
+
+    server.use(express.static(BUILD_PATH, { etag: false })); // etag for turning off caching. not sure if this is the best way to deal with caching
+
+    server.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) => {
+        getDecorator()
+            .then((fragments: any) => {
+                res.render('index.html', fragments);
+            })
+            .catch((e) => {
+                const error = `Failed to get decorator: ${e}`;
+                console.error(error);
+                res.status(500).send(error);
+            });
+    });
 
     server.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../../client/build/index.html'));
