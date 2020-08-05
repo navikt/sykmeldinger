7275a4d5c04b896f66637c24b8b9ee0ecc1821e8
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-08-05 10:10:29 +0200

    Revert "updates decorator"
    This reverts commit 1ed4a8d53fd8f7bcb9e7e57bce3d122dbbca9762.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,28 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import dayjs from 'dayjs';
 
+import withMenu from './mock/decorator/decorator-header-withmenu';
+import megamenu from './mock/decorator/decorator-megamenu';
+import footer from './mock/decorator/decorator-footer';
+import scripts from './mock/decorator/decorator-scripts';
+import skiplinks from './mock/decorator/decorator-skiplinks';
+import styles from './mock/decorator/decorator-styles';
+
 dayjs.locale('nb');
+
+if (process.env.NODE_ENV === 'development' || process.env.REACT_APP_IS_GCP_LABS === 'true') {
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_HEADING}}}', withMenu);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_FOOTER}}}', footer);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_STYLES}}}', styles);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SCRIPTS}}}', scripts);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SKIPLINKS}}}', skiplinks);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{MEGAMENU_RESOURCES}}}', megamenu);
+
+    // Execute client.js
+    var script = document.createElement('script');
+    script.src = 'https://www.nav.no/dekoratoren/client.js';
+    document.body.appendChild(script);
+}
 
 if (process.env.NODE_ENV === 'development' || process.env.REACT_APP_IS_GCP_LABS === 'true') {
     require('./mock/mock');
