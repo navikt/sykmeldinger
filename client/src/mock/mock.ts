9b86ebb722843f3c7456142d8cc0f8d27e038ca1
Sebastian <sebastian.knudsen@nav.no>
2020-08-06 13:29:57 +0200

    Updates (#92)
    * npm updates
    
    * updates to the server packages
    
    * change mock to conform to new version
    
    * fix mock endpoint

--- client/src/mock/mock.ts
+++ client/src/mock/mock.ts
@@ -18,123 +18,323 @@ const mock = FetchMock.configure({
     middleware: MiddlewareUtils.combine(MiddlewareUtils.delayMiddleware(500), MiddlewareUtils.loggingMiddleware()),
 });
 
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmeldinger`, [
-    sykmeldingApen,
-    sykmeldingApenPapir,
-    sykmeldingSendt,
-    sykmeldingBekreftet,
-    sykmeldingAvvist,
-    sykmeldingAvvistBekreftet,
-    sykmeldingAvbrutt,
-    sykmeldingUtgatt,
-    sykmeldingEgenmeldt,
-]);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmeldinger`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            status: 200,
+            body: [
+                sykmeldingApen,
+                sykmeldingApenPapir,
+                sykmeldingSendt,
+                sykmeldingBekreftet,
+                sykmeldingAvvist,
+                sykmeldingAvvistBekreftet,
+                sykmeldingAvbrutt,
+                sykmeldingUtgatt,
+                sykmeldingEgenmeldt,
+            ],
+        }),
+    ),
+);
 
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/APEN`, sykmeldingApen);
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/APEN_PAPIR`, sykmeldingApenPapir);
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/BEKREFTET`, sykmeldingBekreftet);
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/SENDT`, sykmeldingSendt);
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/AVVIST`, sykmeldingAvvist);
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/AVVIST_BEKREFTET`, sykmeldingAvvistBekreftet);
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/AVBRUTT`, sykmeldingAvbrutt);
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/UTGATT`, sykmeldingUtgatt);
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/EGENMELDT`, sykmeldingEgenmeldt);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/APEN`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingApen })),
+);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/APEN_PAPIR`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingApenPapir })),
+);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/BEKREFTET`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingBekreftet })),
+);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/SENDT`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingSendt })),
+);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/AVVIST`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingAvvist })),
+);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/AVVIST_BEKREFTET`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingAvvistBekreftet })),
+);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/AVBRUTT`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingAvbrutt })),
+);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/UTGATT`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingUtgatt })),
+);
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/EGENMELDT`, (_, res, ctx) =>
+    res(ctx.json({ body: sykmeldingEgenmeldt })),
+);
 
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/informasjon/arbeidsgivere`, arbeidsgivereMock);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/informasjon/arbeidsgivere`, (_, res, ctx) =>
+    res(ctx.json({ body: arbeidsgivereMock })),
+);
 
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/APEN/actions/erUtenforVentetid`, {
-    erUtenforVentetid: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/APEN_PAPIR/actions/erUtenforVentetid`, {
-    erUtenforVentetid: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/BEKREFTET/actions/erUtenforVentetid`, {
-    erUtenforVentetid: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/SENDT/actions/erUtenforVentetid`, {
-    erUtenforVentetid: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/AVVIST/actions/erUtenforVentetid`, {
-    erUtenforVentetid: false,
-});
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/APEN/actions/erUtenforVentetid`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
+);
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/APEN_PAPIR/actions/erUtenforVentetid`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
+);
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/BEKREFTET/actions/erUtenforVentetid`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
+);
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/SENDT/actions/erUtenforVentetid`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
+);
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/AVVIST/actions/erUtenforVentetid`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
+);
 mock.get(
     `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/AVVIST_BEKREFTET/actions/erUtenforVentetid`,
-    {
-        erUtenforVentetid: false,
-    },
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
 );
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/AVBRUTT/actions/erUtenforVentetid`, {
-    erUtenforVentetid: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/UTGATT/actions/erUtenforVentetid`, {
-    erUtenforVentetid: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/EGENMELDT/actions/erUtenforVentetid`, {
-    erUtenforVentetid: false,
-});
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/AVBRUTT/actions/erUtenforVentetid`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
+);
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/UTGATT/actions/erUtenforVentetid`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
+);
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/EGENMELDT/actions/erUtenforVentetid`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    erUtenforVentetid: false,
+                },
+            }),
+        ),
+);
 
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=APEN`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=APEN_PAPIR`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=SENDT`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=BEKREFTET`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=AVVIST`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=AVVIST_BEKREFTET`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=AVBRUTT`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=UTGATT`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=EGENMELDT`, {
-    soknaderOpprettet: 0,
-    erBehandlet: false,
-});
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=APEN`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 0,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=APEN_PAPIR`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    soknaderOpprettet: 0,
+                    erBehandlet: false,
+                },
+            }),
+        ),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=SENDT`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 0,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=BEKREFTET`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 0,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=AVVIST`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 0,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
+mock.get(
+    `${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=AVVIST_BEKREFTET`,
+    (_, res, ctx) =>
+        res(
+            ctx.json({
+                body: {
+                    soknaderOpprettet: 0,
+                    erBehandlet: false,
+                },
+            }),
+        ),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=AVBRUTT`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 0,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=UTGATT`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 0,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/soknader/sykmelding-behandlet?sykmeldingId=EGENMELDT`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 0,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
 
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/SENDT`, [soknadNy, soknadFremtidig]);
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/BEKREFTET`, [soknadFremtidig]);
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/APEN`, []);
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/APEN_PAPIR`, []);
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/AVBRUTT`, []);
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/AVVIST`, []);
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/AVVIST_BEKREFTET`, []);
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/UTGATT`, []);
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/EGENMELDT`, []);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/SENDT`, (_, res, ctx) =>
+    res(ctx.json({ body: [soknadNy, soknadFremtidig] })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/BEKREFTET`, (_, res, ctx) =>
+    res(ctx.json({ body: [soknadFremtidig] })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/APEN`, (_, res, ctx) =>
+    res(ctx.json({ body: [] })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/APEN_PAPIR`, (_, res, ctx) =>
+    res(ctx.json({ body: [] })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/AVBRUTT`, (_, res, ctx) =>
+    res(ctx.json({ body: [] })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/AVVIST`, (_, res, ctx) =>
+    res(ctx.json({ body: [] })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/AVVIST_BEKREFTET`, (_, res, ctx) =>
+    res(ctx.json({ body: [] })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/UTGATT`, (_, res, ctx) =>
+    res(ctx.json({ body: [] })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/syfosoknad/sykmelding/EGENMELDT`, (_, res, ctx) =>
+    res(ctx.json({ body: [] })),
+);
 
-mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/actions/gjenapne/AVBRUTT`, {});
+mock.get(`${process.env.REACT_APP_SM_REGISTER_URL}/v1/sykmelding/actions/gjenapne/AVBRUTT`, (_, res, ctx) =>
+    res(ctx.json({ status: 200 })),
+);
 
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN/actions/bekreft`, {});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN_PAPIR/actions/bekreft`, {});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/AVVIST/actions/bekreft`, {});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN/actions/send`, {});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN_PAPIR/actions/send`, {});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN/actions/avbryt`, {});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN_PAPIR/actions/avbryt`, {});
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN/actions/bekreft`, (_, res, ctx) =>
+    res(ctx.json({ status: 200 })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN_PAPIR/actions/bekreft`, (_, res, ctx) =>
+    res(ctx.json({ status: 200 })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/AVVIST/actions/bekreft`, (_, res, ctx) =>
+    res(ctx.json({ status: 200 })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN/actions/send`, (_, res, ctx) =>
+    res(ctx.json({ status: 200 })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN_PAPIR/actions/send`, (_, res, ctx) =>
+    res(ctx.json({ status: 200 })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN/actions/avbryt`, (_, res, ctx) =>
+    res(ctx.json({ status: 200 })),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN_PAPIR/actions/avbryt`, (_, res, ctx) =>
+    res(ctx.json({ status: 200 })),
+);
 
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN/actions/behandlet`, {
-    soknaderOpprettet: 1,
-    erBehandlet: false,
-});
-mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN_PAPIR/actions/behandlet`, {
-    soknaderOpprettet: 1,
-    erBehandlet: false,
-});
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN/actions/behandlet`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 1,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
+mock.get(`${process.env.REACT_APP_SYFOREST_ROOT}/sykmeldinger/APEN_PAPIR/actions/behandlet`, (_, res, ctx) =>
+    res(
+        ctx.json({
+            body: {
+                soknaderOpprettet: 1,
+                erBehandlet: false,
+            },
+        }),
+    ),
+);
