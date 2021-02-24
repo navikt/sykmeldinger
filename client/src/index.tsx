63b9188f5e69977ad68e353344966b2730941eb9
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-02-24 18:31:10 +0100

    remove service worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import 'dayjs/locale/nb';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import dayjs from 'dayjs';
 
 import withMenu from './mock/decorator/decorator-header-withmenu';
@@ -38,5 +37,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('app'),
 );
-
-serviceWorker.unregister();
