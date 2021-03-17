dfe7c94075ce16aa81076d268f97e1b8e7bc9f1b
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-03-17 15:57:49 +0100

    specify path more in shell script

--- process-index-html.sh
+++ process-index-html.sh
@@ -3,7 +3,7 @@
 export TIMESTAMP=$(date +%s)
 echo "Benytter $TIMESTAMP som query param timestamp for static ressurser to prevent caching"
 
-sed -i 's~##DECORATOR_URL##~'"$DECORATOR_URL"'~g' build/index.html
-sed -i 's~##PUBLIC_URL##~'"$PUBLIC_URL"'~g' build/index.html
-sed -i 's~.chunk.css"~'".chunk.css?ts=$TIMESTAMP\""'~g' build/index.html
-sed -i 's~.chunk.js"~'".chunk.js?ts=$TIMESTAMP\""'~g' build/index.html
+sed -i 's~##DECORATOR_URL##~'"$DECORATOR_URL"'~g' ./build/index.html
+sed -i 's~##PUBLIC_URL##~'"$PUBLIC_URL"'~g' ./build/index.html
+sed -i 's~.chunk.css"~'".chunk.css?ts=$TIMESTAMP\""'~g' ./build/index.html
+sed -i 's~.chunk.js"~'".chunk.js?ts=$TIMESTAMP\""'~g' ./build/index.html
