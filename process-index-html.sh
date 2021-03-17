667312056732d3127f09c117be475fd1942e1fb0
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-03-17 16:15:53 +0100

    add logging

--- process-index-html.sh
+++ process-index-html.sh
@@ -3,7 +3,11 @@
 export TIMESTAMP=$(date +%s)
 echo "Benytter $TIMESTAMP som query param timestamp for static ressurser to prevent caching"
 
+ls -al ./build
+
 sed -i 's~##DECORATOR_URL##~'"$DECORATOR_URL"'~g' ./build/index.html
 sed -i 's~##PUBLIC_URL##~'"$PUBLIC_URL"'~g' ./build/index.html
 sed -i 's~.chunk.css"~'".chunk.css?ts=$TIMESTAMP\""'~g' ./build/index.html
 sed -i 's~.chunk.js"~'".chunk.js?ts=$TIMESTAMP\""'~g' ./build/index.html
+
+cat ./build/index.html
