b17f0f36e508df12652f9ab028be9575fcaee548
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-03-24 09:56:56 +0100

    Correct endpoint for erUtenforVentetid

--- src/pages/commonComponents/hooks/useSykmeldingUtenforVentetid.ts
+++ src/pages/commonComponents/hooks/useSykmeldingUtenforVentetid.ts
@@ -3,15 +3,16 @@ import { authenticatedGet } from '../../../utils/fetchUtils';
 
 function useSykmeldingUtenforVentetid(sykmeldingId: string) {
     return useQuery<boolean, Error>(['erUtenforVentetid', sykmeldingId], () =>
-        // TODO: correct endpoint
-        // TODO: make Class for respons
         authenticatedGet(
-            `${window._env_?.SYFOREST_ROOT}/syfosoknad/api/sykmeldinger/${sykmeldingId}/actions/erUtenforVentetid`,
+            `${window._env_?.FLEX_GATEWAY_ROOT}/syfosoknad/api/sykmeldinger/${sykmeldingId}/actions/erUtenforVentetid`,
             (data) => {
-                if (typeof (data as any).erUtenforVentetid === 'boolean') {
-                    return Boolean((data as any).erUtenforVentetid);
+                const maybeErUtenforVentetid = (data as any).erUtenforVentetid;
+                if (typeof maybeErUtenforVentetid === 'boolean') {
+                    return Boolean(maybeErUtenforVentetid);
                 }
-                throw new Error();
+                throw new TypeError(
+                    `Property erUtenforVentetid of type ${typeof maybeErUtenforVentetid} is not assignable to type boolean`,
+                );
             },
         ),
     );
