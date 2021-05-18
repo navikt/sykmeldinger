9f1f8db918da003a7b3f5d32e57a839f69005522
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-05-18 11:52:24 +0200

    use location hook instead of window.location

--- src/hooks/useSend.ts
+++ src/hooks/useSend.ts
@@ -1,14 +1,14 @@
 import { useMutation, useQueryClient } from 'react-query';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { FormShape } from '../pages/sykmelding/OK/APEN/Form/Form';
 import Fetch from '../utils/Fetch';
 
 function useSend(sykmeldingId: string) {
     const queryClient = useQueryClient();
     const history = useHistory();
+    const { pathname } = useLocation();
 
     return useMutation(
-        // TODO: endpoint is not implemented at sykmeldinger-backend
         (values: FormShape) =>
             Fetch.authenticatedPost(
                 `${window._env_?.SYKMELDINGER_BACKEND_PROXY_ROOT}/api/v2/sykmeldinger/${sykmeldingId}/send`,
@@ -18,7 +18,7 @@ function useSend(sykmeldingId: string) {
             onSuccess: () => {
                 queryClient.invalidateQueries('sykmeldinger');
                 queryClient.invalidateQueries(['sykmelding', sykmeldingId]);
-                history.push(window.location.pathname + '/kvittering');
+                history.push(pathname + '/kvittering');
                 window.scrollTo(0, 0);
             },
         },
