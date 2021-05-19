efc122b00de1edad7b82d09071638b0c87c0edac
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-05-19 08:54:15 +0200

    Refactor avvist sykmelding veileder info

--- src/pages/sykmelding/INVALID/VeilederContent.tsx
+++ src/pages/sykmelding/INVALID/VeilederContent.tsx
@@ -4,26 +4,13 @@ import React from 'react';
 import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
 
 import { Sykmelding } from '../../../models/Sykmelding/Sykmelding';
-import Behandler from '../../../models/Sykmelding/Behandler';
 
 interface VeilederContentProps {
     sykmelding: Sykmelding;
 }
 
 const VeilederContent: React.FC<VeilederContentProps> = ({ sykmelding }) => {
-    const createLegenavn = (behandler: Behandler) => {
-        const { fornavn, mellomnavn, etternavn } = behandler;
-        let navn = fornavn;
-        if (mellomnavn) {
-            navn += ` ${mellomnavn}`;
-        }
-        if (etternavn) {
-            navn += ` ${etternavn}`;
-        }
-        return navn;
-    };
-
-    const legenavn = createLegenavn(sykmelding.behandler);
+    const behandlerNavn = sykmelding.behandler.getName();
 
     return (
         <>
@@ -32,14 +19,14 @@ const VeilederContent: React.FC<VeilederContentProps> = ({ sykmelding }) => {
             <Normaltekst>Beklager at vi må bry deg mens du er syk.</Normaltekst>
             <br />
             <Normaltekst>
-                Du trenger en ny sykmelding fordi det er gjort en feil i utfyllingen. Vi har gitt beskjed til {legenavn}{' '}
-                om hva som er feil, og at du må få en ny sykmelding.
+                Du trenger en ny sykmelding fordi det er gjort en feil i utfyllingen. Vi har gitt beskjed til{' '}
+                {behandlerNavn} om hva som er feil, og at du må få en ny sykmelding.
             </Normaltekst>
             <br />
             <Normaltekst>
-                Når du har fått ny sykmelding fra {legenavn}, får du en ny beskjed fra oss om å logge deg inn på nav.no
-                slik at du kan sende inn sykmeldingen. Går det mange dager, bør du kontakte {legenavn} som skal skrive
-                den nye sykmeldingen.
+                Når du har fått ny sykmelding fra {behandlerNavn}, får du en ny beskjed fra oss om å logge deg inn på
+                nav.no slik at du kan sende inn sykmeldingen. Går det mange dager, bør du kontakte {behandlerNavn} som
+                skal skrive den nye sykmeldingen.
             </Normaltekst>
             <br />
             <Element>Grunnen til at sykmeldingen er avvist:</Element>
