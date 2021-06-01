1c64c2a4500682d10c75ed8034787a2ef361fc4c
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-06-01 16:00:24 +0200

    Disable refetchOnWindowFocus for all queries to lessen the load on the server

--- src/hooks/useSykmeldingUtenforVentetid.ts
+++ src/hooks/useSykmeldingUtenforVentetid.ts
@@ -4,25 +4,22 @@ import ErUtenforVentetid from '../models/ErUtenforVentetid';
 import Fetch from '../utils/Fetch';
 
 function useSykmeldingUtenforVentetid(sykmeldingId: string) {
-    return useQuery<ErUtenforVentetid, Error>(
-        ['erUtenforVentetid', sykmeldingId],
-        () =>
-            Fetch.authenticatedGet(
-                `${window._env_?.FLEX_GATEWAY_ROOT}/syfosoknad/api/sykmeldinger/${sykmeldingId}/actions/v2/erUtenforVentetid`,
-                async (maybeErUtenforVentetid) => {
-                    const erUtenforVentetid = new ErUtenforVentetid(maybeErUtenforVentetid);
-                    await validateOrReject(erUtenforVentetid, { validationError: { target: false, value: false } });
-                    return erUtenforVentetid;
-                },
-            ).then((value) => {
-                if (value.erUtenforVentetid === false && !value.oppfolgingsdato) {
-                    console.warn(
-                        `Expected oppfolgingsdato to be defined when sykmelding within ventetid, but was ${typeof value.oppfolgingsdato}. Sykmeldingid: ${sykmeldingId}`,
-                    );
-                }
-                return value;
-            }),
-        { refetchOnWindowFocus: false },
+    return useQuery<ErUtenforVentetid, Error>(['erUtenforVentetid', sykmeldingId], () =>
+        Fetch.authenticatedGet(
+            `${window._env_?.FLEX_GATEWAY_ROOT}/syfosoknad/api/sykmeldinger/${sykmeldingId}/actions/v2/erUtenforVentetid`,
+            async (maybeErUtenforVentetid) => {
+                const erUtenforVentetid = new ErUtenforVentetid(maybeErUtenforVentetid);
+                await validateOrReject(erUtenforVentetid, { validationError: { target: false, value: false } });
+                return erUtenforVentetid;
+            },
+        ).then((value) => {
+            if (value.erUtenforVentetid === false && !value.oppfolgingsdato) {
+                console.warn(
+                    `Expected oppfolgingsdato to be defined when sykmelding within ventetid, but was ${typeof value.oppfolgingsdato}. Sykmeldingid: ${sykmeldingId}`,
+                );
+            }
+            return value;
+        }),
     );
 }
 
