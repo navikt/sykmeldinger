328041a71399a0ce55c9b3c1af883c5d844eac56
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-06-09 12:48:48 +0200

    do not show begrunnelseIkkeKontakt for arbeidsgiver

--- src/components/Sykmeldingview/Sections/TilbakedateringView.test.tsx
+++ src/components/Sykmeldingview/Sections/TilbakedateringView.test.tsx
@@ -24,10 +24,19 @@ describe('TilbakedateringView', () => {
         const kontaktMedPasient = new KontaktMedPasient(plainJson);
         render(<TilbakedateringView kontaktMedPasient={kontaktMedPasient} />);
 
-        expect(screen.getByText('Tilbakedatering')).toBeInTheDocument();
-        expect(screen.getByText('Dato for dokumenterbar kontakt med pasienten')).toBeInTheDocument();
-        expect(screen.getByText('1. april 2021')).toBeInTheDocument();
         expect(screen.getByText('Begrunnelse for tilbakedatering')).toBeInTheDocument();
         expect(screen.getByText(plainJson.begrunnelseIkkeKontakt)).toBeInTheDocument();
+    });
+
+    it('Does not render begrunnelse for arbeidsgiver', () => {
+        const plainJson = {
+            kontaktDato: '2021-04-01',
+            begrunnelseIkkeKontakt: 'han var kjempesyk',
+        };
+        const kontaktMedPasient = new KontaktMedPasient(plainJson);
+        render(<TilbakedateringView kontaktMedPasient={kontaktMedPasient} arbeidsgiver />);
+
+        expect(screen.queryByText('Begrunnelse for tilbakedatering')).not.toBeInTheDocument();
+        expect(screen.queryByText(plainJson.begrunnelseIkkeKontakt)).not.toBeInTheDocument();
     });
 });
