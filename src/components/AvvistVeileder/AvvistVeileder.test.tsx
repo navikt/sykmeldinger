568213139eef88b1a326d5e32f9011b9eaebe76b
Natalie Uranes <natalie.uranes@nav.no>
2021-10-08 09:39:40 +0200

    Add tests

--- src/components/AvvistVeileder/AvvistVeileder.test.tsx
+++ src/components/AvvistVeileder/AvvistVeileder.test.tsx
@@ -4,6 +4,23 @@ import Behandlingsutfall from '../../models/Sykmelding/Behandlingsutfall';
 import AvvistVeileder from './AvvistVeileder';
 
 describe('AvvistVeileder', () => {
+    it('Renders custom message if the therapist is missing authorization', () => {
+        const plainBehandlingsutfall = {
+            status: 'INVALID',
+            ruleHits: [
+                {
+                    messageForSender: '',
+                    messageForUser: '',
+                    ruleName: 'BEHANDLER_MANGLER_AUTORISASJON_I_HPR',
+                    ruleStatus: 'INVALID',
+                },
+            ],
+        };
+        const behandlingsutfall = new Behandlingsutfall(plainBehandlingsutfall);
+        render(<AvvistVeileder behandlerNavn={'Doktor Legesen'} behandlingsutfall={behandlingsutfall} />);
+        expect(screen.getByText(/Den som har skrevet sykmeldingen, har ikke autorisasjon/)).toBeInTheDocument();
+    });
+
     it('Renders custom message for people over 70', () => {
         const plainBehandlingsutfall = {
             status: 'INVALID',
@@ -22,7 +39,24 @@ describe('AvvistVeileder', () => {
         expect(screen.getByText(/Du har ikke rett til sykepenger fordi du er over 70 år/)).toBeInTheDocument();
     });
 
-    it('Renders normal message for other rulehits than PASIENT_ELDRE_ENN_70', () => {
+    it('Renders custom message if z diagnose', () => {
+        const plainBehandlingsutfall = {
+            status: 'INVALID',
+            ruleHits: [
+                {
+                    messageForSender: '',
+                    messageForUser: '',
+                    ruleName: 'ICPC_2_Z_DIAGNOSE',
+                    ruleStatus: 'INVALID',
+                },
+            ],
+        };
+        const behandlingsutfall = new Behandlingsutfall(plainBehandlingsutfall);
+        render(<AvvistVeileder behandlerNavn={'Doktor Legesen'} behandlingsutfall={behandlingsutfall} />);
+        expect(screen.getByText(/Legen har skrevet en diagnose i sykmeldingen som ikke gir deg rett til å få sykepenger./)).toBeInTheDocument();
+    });
+
+    it('Renders normal message for other rulehits', () => {
         const plainBehandlingsutfall = {
             status: 'INVALID',
             ruleHits: [
