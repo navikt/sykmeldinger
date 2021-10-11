c0e7bfdd5f77e563ed611545ccf762dd15660be2
Natalie Uranes <natalie.uranes@nav.no>
2021-10-11 12:50:56 +0200

    Add new paragraph for line break in z diagnosis

--- src/components/AvvistVeileder/ForklaringZDiagnose.tsx
+++ src/components/AvvistVeileder/ForklaringZDiagnose.tsx
@@ -6,8 +6,8 @@ const ForklaringZDiagnose = () => {
             <Normaltekst>
                 Legen har skrevet en diagnose i sykmeldingen som ikke gir deg rett til å få sykepenger.
                 Hvis du mener det er feil, kan du kontakte legen din.
-                Du kan også kontakte arbeidsgiveren din og høre om du kan bruke egenmelding eller velferdspermisjon i stedet.
             </Normaltekst>
+            <Normaltekst>Du kan også kontakte arbeidsgiveren din og høre om du kan bruke egenmelding eller velferdspermisjon i stedet.</Normaltekst>
         </>
     );
 };
