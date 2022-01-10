84b0843f1d73f23068f0c5b22101523482aeecb0
Natalie Uranes <natalie.uranes@nav.no>
2022-01-10 13:26:03 +0100

    Remove info icon on panel for digital sykmelding

--- src/components/InfoOmDigitalSykmelding/InfoOmDigitalSykmelding.tsx
+++ src/components/InfoOmDigitalSykmelding/InfoOmDigitalSykmelding.tsx
@@ -4,29 +4,11 @@ import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import env from '../../utils/env';
 import './InfoOmDigitalSykmelding.less';
 
-const InfoSvg: React.FC = () => (
-    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M15 0C6.72783 0 0 6.72783 0 15C0 23.2722 6.72783 30 15 30C23.2709 30 30 23.2722 30 15C30 6.72783 23.2709 0 15 0Z"
-            fill="#5690A2"
-        />
-        <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M16.9563 8.20652C16.9563 7.12652 16.0778 6.25 14.9998 6.25C13.9178 6.25 13.0433 7.12652 13.0433 8.20652C13.0433 9.28848 13.9178 10.163 14.9998 10.163C16.0778 10.163 16.9563 9.28848 16.9563 8.20652ZM20.0236 22.5006C20.0236 21.8004 19.424 21.2615 18.7193 21.2615H16.3468V13.7538C16.3468 13.0536 15.7471 12.5146 15.0424 12.5146H12.5058C11.8 12.5146 11.2014 13.0534 11.2014 13.7538C11.2014 14.4542 11.8 14.9929 12.5058 14.9929H13.7381V21.2615H11.7389C11.0332 21.2615 10.4346 21.8002 10.4346 22.5006C10.4346 23.201 11.0332 23.7397 11.7389 23.7397H18.7193C19.424 23.7397 20.0236 23.2008 20.0236 22.5006Z"
-            fill="white"
-        />
-    </svg>
-);
-
 const InfoOmDigitalSykmelding: React.FC = () => (
     <Ekspanderbartpanel
         className="info-om-digital-sykmelding"
         tittel={
             <div className="info-om-digital-sykmelding__title">
-                <InfoSvg />
                 <Undertittel tag="h2">Om den digitale sykmeldingen</Undertittel>
             </div>
         }
