639fe552e1c0e83d1cab82f8310d462a5c27940d
Natalie Uranes <natalie.uranes@nav.no>
2022-01-12 12:04:45 +0100

    Add Lukk button to Flere opplysninger

--- src/components/Sykmeldingview/FlereOpplysninger.test.tsx
+++ src/components/Sykmeldingview/FlereOpplysninger.test.tsx
@@ -3,7 +3,7 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import FlereOpplysninger from './FlereOpplysninger';
 
 describe('Flere opplysninger', () => {
-    it('Does not render content when button is not clicked in Ekspanderbar', () => {
+    it('Does not render content when button is not clicked in EkspanderbartpanelBase', () => {
         render(
             <FlereOpplysninger>
                 <p>Hello world</p>
@@ -13,7 +13,7 @@ describe('Flere opplysninger', () => {
         expect(screen.getByText('Vis flere opplysninger')).toBeInTheDocument();
     });
 
-    it('Renders content and change button text when button is clicked in Ekspanderbar', async () => {
+    it('Renders content and change button text when button is clicked in EkspanderbartpanelBase', async () => {
         render(
             <FlereOpplysninger>
                 <p>Hello world</p>
@@ -28,7 +28,7 @@ describe('Flere opplysninger', () => {
         expect(screen.getByText('Vis flere opplysninger')).toBeInTheDocument();
     });
 
-    it('Render content and not Ekspanderbar if disableExpand is true', () => {
+    it('Render content and not EkspanderbartpanelBase if disableExpand is true', () => {
         render(
             <FlereOpplysninger disableExpand={true}>
                 <p>Hello world</p>
@@ -38,4 +38,21 @@ describe('Flere opplysninger', () => {
         expect(screen.queryByText('Vis flere opplysninger')).not.toBeInTheDocument();
         expect(screen.queryByText('Skjul flere opplysninger')).not.toBeInTheDocument();
     });
+});
+
+it('Close panel if Lukk button is clicked', async () => {
+    render(
+        <FlereOpplysninger>
+            <p>Information</p>
+        </FlereOpplysninger>
+    );
+    expect(screen.queryByText('Information')).not.toBeInTheDocument();
+    expect(screen.queryByText('Lukk')).not.toBeInTheDocument();
+    fireEvent.click(screen.getByText('Vis flere opplysninger'));
+    await screen.findByText('Information');
+    expect(screen.getByText('Skjul flere opplysninger')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Lukk'));
+    expect(screen.getByText('Vis flere opplysninger')).toBeInTheDocument();
+    expect(screen.getByText('Information')).toBeInTheDocument();
 });
