e291efaeebfc5501187d3353aabb1dde0bfd6b68
Karl J. Overå <k@rl.run>
2022-07-08 13:08:20 +0200

    log date when date is invalid

--- src/server/api-models/date.ts
+++ src/server/api-models/date.ts
@@ -1,6 +1,17 @@
 import { z } from 'zod';
 import { parseISO, isValid } from 'date-fns';
 
-export const LocalDateSchema = z.string().refine((date) => isValid(parseISO(date)), {
-    message: 'Invalid date string',
-});
+import { logger } from '../../utils/logger';
+
+export const LocalDateSchema = z.string().refine(
+    (date) => {
+        const valid = isValid(parseISO(date));
+        if (!valid) {
+            logger.error(`Strange invalid date!!, date is: ${date}`);
+        }
+        return valid;
+    },
+    {
+        message: 'Invalid date string',
+    },
+);
