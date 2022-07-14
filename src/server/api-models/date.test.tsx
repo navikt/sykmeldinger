e719370426d82e93e705159e8d51ade1156952c9
Natalie Uranes <natalie.uranes@nav.no>
2022-07-14 12:15:02 +0200

    parse date with additional number in year

---
+++ src/server/api-models/date.test.tsx
@@ -0,0 +1,13 @@
+import { parseISO, isValid } from 'date-fns';
+
+describe('date', () => {
+    it('should parse date', () => {
+        const date = '2021-12-01';
+        expect(isValid(parseISO(date, { additionalDigits: 1 }))).toBe(true);
+    });
+
+    it('should parse date with additional number of digit in extended year format', () => {
+        const date = '+92020-01-09';
+        expect(isValid(parseISO(date, { additionalDigits: 1 }))).toBe(true);
+    });
+});
