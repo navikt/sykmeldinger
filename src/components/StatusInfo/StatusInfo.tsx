2bee4ae3ab377c11f80fc9e94da0c87b65915c03
Keourac <Bendik.Berntsen@nav.no>
2022-12-12 15:53:29 +0100

    lagt til info om reise utenfor EØS i kvitteringen

--- src/components/StatusInfo/StatusInfo.tsx
+++ src/components/StatusInfo/StatusInfo.tsx
@@ -1,13 +1,10 @@
-import { BodyLong, BodyShort, GuidePanel, Link } from '@navikt/ds-react'
+import { BodyShort, GuidePanel, Link } from '@navikt/ds-react'
 
 import { Merknad, Periode, Periodetype, SykmeldingStatus } from '../../fetching/graphql.generated'
 import Spacing from '../Spacing/Spacing'
 import { Merknadtype } from '../InformationBanner/InformationBanner'
-import { getPublicEnv } from '../../utils/env'
 
 import styles from './StatusInfo.module.css'
-
-const publicEnv = getPublicEnv()
 
 interface StatusInfoProps {
     sykmeldingStatus: SykmeldingStatus
@@ -42,17 +39,17 @@ function StatusInfo({
             <div data-testid="status-info">
                 <GuidePanel poster>
                     <Spacing amount="small">
-                        <BodyLong>
+                        <BodyShort>
                             Du har sendt beskjed til arbeidsgiveren din om at det er mulig å unngå sykmelding hvis det
                             blir lagt til rette for deg på arbeidsplassen.
-                        </BodyLong>
+                        </BodyShort>
                     </Spacing>
                     <Spacing amount="small">
-                        <BodyLong>
+                        <BodyShort>
                             Husk at du har mulighet til å lage en digital oppfølgingsplan sammen med arbeidsgiveren din.
                             Hensikten er å finne ut hvilke oppgaver du kan gjøre hvis lederen din legger til rette for
                             det.
-                        </BodyLong>
+                        </BodyShort>
                     </Spacing>
                     <a href="/oppfolgingsplan/oppfolgingsplaner" className="knapp">
                         Lag en oppfølgingsplan
@@ -67,18 +64,18 @@ function StatusInfo({
             <div data-testid="status-info" className={styles.root}>
                 <GuidePanel poster>
                     <Spacing amount="small">
-                        <BodyLong>Vanligvis fyller du ut en søknad om sykepenger når sykmeldingen er over.</BodyLong>
+                        <BodyShort>Vanligvis fyller du ut en søknad om sykepenger når sykmeldingen er over.</BodyShort>
                     </Spacing>
                     <Spacing amount="small">
-                        <BodyLong>
+                        <BodyShort>
                             Siden legen har skrevet at sykmeldingen startet før dere hadde kontakt, må NAV først vurdere
                             om det var en gyldig grunn til dette.
-                        </BodyLong>
+                        </BodyShort>
                     </Spacing>
                     <Spacing amount="small">
-                        <BodyLong>
+                        <BodyShort>
                             Du får en melding fra oss når vurderingen er ferdig og søknaden er klar til utfylling.
-                        </BodyLong>
+                        </BodyShort>
                     </Spacing>
                 </GuidePanel>
             </div>
@@ -89,25 +86,28 @@ function StatusInfo({
         <div data-testid="status-info">
             <GuidePanel poster>
                 <Spacing amount="small">
-                    <BodyLong>
+                    <BodyShort>
                         Når sykefraværet er over, får du en melding fra oss igjen. Da svarer du på noen spørsmål slik at
                         vi kan beregne sykepengene dine riktig.
-                    </BodyLong>
+                    </BodyShort>
                 </Spacing>
                 <Spacing amount="small">
-                    <BodyLong>
-                        Du kan kikke på det allerede nå i{' '}
-                        <Link href={publicEnv.SYKEPENGESOKNAD_URL || '#'}>dine søknader</Link>.
-                    </BodyLong>
+                    <BodyShort>
+                        Hvis du vurderer å reise utenfor EØS mens du er sykmeldt, må du{' '}
+                        <Link href="https://tjenester.nav.no/sykepengesoknad/sykepengesoknad-utland" target="_blank">
+                            søke om å beholde sykepengene
+                        </Link>{' '}
+                        før du reiser.
+                    </BodyShort>
                 </Spacing>
 
                 {erFlEllerSn && (
                     <Spacing amount="small">
-                        <BodyLong>
+                        <BodyShort>
                             Husk at NAV ikke dekker sykepenger de første 16 dagene av sykefraværet, med mindre du har
                             tegnet forsikring. Har du ikke forsikring, trenger du ikke levere søknad hvis sykefraværet
                             er kortere enn 17 dager.
-                        </BodyLong>
+                        </BodyShort>
                     </Spacing>
                 )}
 
