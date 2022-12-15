66126b2e73ae6781728e499e1f85bf7c984340fe
Karl J. Overå <k@rl.run>
2022-12-15 13:46:27 +0100

    [skip ci] update dept.sh

--- scripts/debt.sh
+++ scripts/debt.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 fc=$(grep -roh ".FC" src | wc -w)
-any=$(grep -roh "any" src | wc -w)
+any=$(grep -roh ": any" src | wc -w)
 todo=$(grep -roh "TODO" src | wc -w)
 dayjs=$(grep -roh "dayjs" src | wc -w)
 
 echo -e "### Gjenstående opprydding:"
 echo -e " * $fc React.FC bruk"
 echo -e " * $any anys"
+echo -e " * $todo TODOs"
 echo -e " * $dayjs dayjs imports"
