89ba4eafb004383555044aa87b477626946f7021
Karl J. Overå <k@rl.run>
2022-12-21 16:09:23 +0100

    add amplitude event for sykmelding page

--- src/amplitude/taxonomyEvents.ts
+++ src/amplitude/taxonomyEvents.ts
@@ -21,3 +21,5 @@ export type AmplitudeTaxonomyEvents =
     | { eventName: 'skjema validering feilet'; data: { skjemanavn: string /* skjemaId: number */ } }
     | { eventName: 'skjema åpnet'; data: { skjemanavn: string /* skjemaId: number */ } }
     | { eventName: 'guidepanel vist'; data: { komponent: string; tekst?: string } }
+    // Non-standard event
+    | { eventName: 'komponent vist'; data: { komponent: string } }
