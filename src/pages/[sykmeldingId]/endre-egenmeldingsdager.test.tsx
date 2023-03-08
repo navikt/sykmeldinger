43d4aca2397fb5cf057ca72ce7e6e2b53b6847bb
Karl J. Overå <k@rl.run>
2023-03-08 09:55:18 +0100

    test display of existing periods on endre egenmeldingsdager page

--- src/pages/[sykmeldingId]/endre-egenmeldingsdager.test.tsx
+++ src/pages/[sykmeldingId]/endre-egenmeldingsdager.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import mockRouter from 'next-router-mock'
-import { logRoles } from '@testing-library/react'
 
 import { render, screen } from '../../utils/test/testUtils'
 import {
@@ -17,6 +16,7 @@ import {
     Svartype,
     SykmeldingByIdDocument,
     SykmeldingerDocument,
+    SykmeldingFragment,
     SykmeldingStatusFragment,
 } from '../../fetching/graphql.generated'
 
@@ -33,51 +33,171 @@ describe('endre egenmeldingsdager page', () => {
         mockRouter.setCurrentUrl(`/sykmelding-id/endre-egenmeldingsdager`)
     })
 
-    it('should display egenmelding form with the correct responses given a single period', async () => {
-        const sykmelding = createSykmelding({
-            id: 'sykmelding-id',
-            sykmeldingsperioder: [
-                createSykmeldingPeriode({
-                    type: Periodetype.AKTIVITET_IKKE_MULIG,
-                    fom: '2020-05-01',
-                    tom: '2020-05-10',
-                }),
-            ],
-            sykmeldingStatus: createSykmeldingStatus({
-                statusEvent: StatusEvent.SENDT,
-                arbeidsgiver,
-                sporsmalOgSvarListe: [createEgenmeldingsdagerSporsmal(['2020-04-28', '2020-04-29'])],
-            }),
-        })
+    function setup(input: SykmeldingFragment | SykmeldingFragment[], sykmeldingByIdIndex = 0): void {
+        const sykmeldinger = Array.isArray(input) ? input : [input]
 
         render(<EndreEgenmeldingsdagerPage />, {
             mocks: [
                 createMock({
                     request: { query: SykmeldingByIdDocument, variables: { id: 'sykmelding-id' } },
-                    result: { data: { __typename: 'Query', sykmelding: sykmelding } },
+                    result: { data: { __typename: 'Query', sykmelding: sykmeldinger[sykmeldingByIdIndex] } },
                 }),
                 createMock({
                     request: { query: SykmeldingerDocument },
-                    result: { data: { __typename: 'Query', sykmeldinger: [sykmelding] } },
+                    result: { data: { __typename: 'Query', sykmeldinger: sykmeldinger } },
                 }),
             ],
         })
+    }
 
-        expect(
-            await screen.findRadioInGroup(
-                { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 15. - 30. april 2020/ },
-                { name: 'Ja' },
-            ),
-        ).toBeInTheDocument()
-        expect(
-            screen.getRadioInGroup(
-                { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 12. - 14. april 2020/ },
-                { name: 'Ja' },
-            ),
-        ).toBeInTheDocument()
+    describe('when displaying existing egenmeldingsdager', () => {
+        it('shall correctly handle a single period', async () => {
+            const sykmelding = createSykmelding({
+                id: 'sykmelding-id',
+                sykmeldingsperioder: [
+                    createSykmeldingPeriode({
+                        type: Periodetype.AKTIVITET_IKKE_MULIG,
+                        fom: '2020-05-01',
+                        tom: '2020-05-10',
+                    }),
+                ],
+                sykmeldingStatus: createSykmeldingStatus({
+                    statusEvent: StatusEvent.SENDT,
+                    arbeidsgiver,
+                    sporsmalOgSvarListe: [createEgenmeldingsdagerSporsmal(['2020-04-28', '2020-04-29'])],
+                }),
+            })
 
-        logRoles(document.body)
-        screen.openPlayground()
+            setup(sykmelding)
+
+            expect(
+                await screen.findRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 15. - 30. april 2020/ },
+                    { name: 'Ja' },
+                ),
+            ).toBeChecked()
+            expect(screen.getByRole('list', { name: 'Du brukte 2 egenmeldingsdager' })).toHaveTextContent(
+                /28. april 2020(.*)29. april 2020/,
+            )
+
+            expect(
+                screen.getRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 12. - 14. april 2020/ },
+                    { name: 'Nei' },
+                ),
+            ).toBeChecked()
+        })
+
+        it('shall correctly handle a two periods', async () => {
+            const sykmelding = createSykmelding({
+                id: 'sykmelding-id',
+                sykmeldingsperioder: [
+                    createSykmeldingPeriode({
+                        type: Periodetype.AKTIVITET_IKKE_MULIG,
+                        fom: '2020-05-01',
+                        tom: '2020-05-10',
+                    }),
+                ],
+                sykmeldingStatus: createSykmeldingStatus({
+                    statusEvent: StatusEvent.SENDT,
+                    arbeidsgiver,
+                    sporsmalOgSvarListe: [createEgenmeldingsdagerSporsmal(['2020-04-12', '2020-04-28', '2020-04-29'])],
+                }),
+            })
+
+            setup(sykmelding)
+
+            expect(
+                await screen.findRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 15. - 30. april 2020/ },
+                    { name: 'Ja' },
+                ),
+            ).toBeChecked()
+            expect(screen.getByRole('list', { name: 'Du brukte 2 egenmeldingsdager' })).toHaveTextContent(
+                /28. april 2020(.*)29. april 2020/,
+            )
+
+            expect(
+                screen.getRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 12. - 14. april 2020/ },
+                    { name: 'Ja' },
+                ),
+            ).toBeChecked()
+            expect(screen.getByRole('list', { name: 'Du brukte 1 egenmeldingsdag' })).toHaveTextContent(
+                /12. april 2020/,
+            )
+
+            expect(
+                screen.getRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 27. mars - 11. april 2020/ },
+                    { name: 'Nei' },
+                ),
+            ).toBeChecked()
+        })
+
+        it('shall correctly handle a three periods', async () => {
+            const sykmelding = createSykmelding({
+                id: 'sykmelding-id',
+                sykmeldingsperioder: [
+                    createSykmeldingPeriode({
+                        type: Periodetype.AKTIVITET_IKKE_MULIG,
+                        fom: '2020-05-01',
+                        tom: '2020-05-10',
+                    }),
+                ],
+                sykmeldingStatus: createSykmeldingStatus({
+                    statusEvent: StatusEvent.SENDT,
+                    arbeidsgiver,
+                    sporsmalOgSvarListe: [
+                        createEgenmeldingsdagerSporsmal([
+                            '2020-03-26',
+                            '2020-04-02',
+                            '2020-04-12',
+                            '2020-04-15',
+                            '2020-04-18',
+                            '2020-04-29',
+                        ]),
+                    ],
+                }),
+            })
+
+            setup(sykmelding)
+
+            expect(
+                await screen.findRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 15. - 30. april 2020/ },
+                    { name: 'Ja' },
+                ),
+            ).toBeChecked()
+            expect(screen.getByRole('list', { name: 'Du brukte 3 egenmeldingsdager' })).toHaveTextContent(
+                /15. april 2020(.*)18. april 2020(.*)29. april 2020/,
+            )
+
+            expect(
+                screen.getRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 30. mars - 14. april 2020/ },
+                    { name: 'Ja' },
+                ),
+            ).toBeChecked()
+            expect(screen.getByRole('list', { name: 'Du brukte 2 egenmeldingsdager' })).toHaveTextContent(
+                /2. april 2020(.*)12. april 2020/,
+            )
+
+            expect(
+                screen.getRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 17. - 29. mars 2020/ },
+                    { name: 'Ja' },
+                ),
+            ).toBeChecked()
+            expect(screen.getByRole('list', { name: 'Du brukte 1 egenmeldingsdag' })).toHaveTextContent(/26. mars 2020/)
+
+            expect(
+                screen.getRadioInGroup(
+                    { name: /Brukte du egenmelding hos Arbeidsgiver AS i perioden 10. - 16. mars 2020?/ },
+                    { name: 'Nei' },
+                ),
+            ).toBeChecked()
+        })
     })
 })
 
