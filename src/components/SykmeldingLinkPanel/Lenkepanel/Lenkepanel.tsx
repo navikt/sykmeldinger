64cb8d445f940b347b0cbfddcf9f95070731c463
Karl J. Overå <k@rl.run>
2023-04-21 09:43:21 +0200

    don't shrink etikett on Lenkepanel

--- src/components/SykmeldingLinkPanel/Lenkepanel/Lenkepanel.tsx
+++ src/components/SykmeldingLinkPanel/Lenkepanel/Lenkepanel.tsx
@@ -48,7 +48,7 @@ export function Lenkepanel({ sykmelding, notifying }: LenkepanelProps): JSX.Elem
                             ))}
                         </ul>
                     </div>
-                    <div className="flex items-center">
+                    <div className="flex shrink-0 items-center">
                         <LenkepanelEtikett status={status} behandlingsutfall={behandlingsutfallStatus} />
                     </div>
                 </div>
