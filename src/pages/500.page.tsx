de33b9a6c43a896a3d2ce3a8d3d6ec32d2a11977
Karl J. Overå <k@rl.run>
2023-04-21 11:13:14 +0200

    add 500 error page

---
+++ src/pages/500.page.tsx
@@ -0,0 +1,28 @@
+import { BodyShort, GuidePanel, Heading, Link } from '@navikt/ds-react'
+import React from 'react'
+
+import { useUpdateBreadcrumbs } from '../hooks/useBreadcrumbs'
+import PageWrapper from '../components/PageWrapper/PageWrapper'
+
+const NotFoundPage = (): JSX.Element => {
+    useUpdateBreadcrumbs(() => [{ title: 'Ukjent feil' }])
+
+    return (
+        <PageWrapper>
+            <GuidePanel poster className="mt-8">
+                <Heading level="2" size="small" spacing>
+                    En ukjent feil har oppstått
+                </Heading>
+                <BodyShort>
+                    Dersom feilen fortsetter, er det fint om du sier i fra til oss på{' '}
+                    <Link href="https://www.nav.no/person/kontakt-oss/nb/tilbakemeldinger/feil-og-mangler">
+                        skjemaet for feil og mangler
+                    </Link>
+                    .
+                </BodyShort>
+            </GuidePanel>
+        </PageWrapper>
+    )
+}
+
+export default NotFoundPage
