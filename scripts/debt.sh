d56cefd34f19a2b2d000d71c1adc750cddb17d6e
Karl J. Overå <k@rl.run>
2023-04-27 09:43:48 +0200

    remove unused code

--- scripts/debt.sh
+++ scripts/debt.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-any=$(grep -roh ": any" src | wc -w)
 todo=$(grep -roh "TODO" src | wc -w)
 
 echo -e "### Gjenstående opprydding:"
-echo -e " * $any anys"
-echo -e " * $todo TODOs"
+echo -e " * $todo TODOs i koden"
