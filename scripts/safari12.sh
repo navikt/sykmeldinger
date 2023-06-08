1ec8a0f577e37e7fd78822328ff2f12bc83ed0d0
Karl J. Overå <k@rl.run>
2023-06-08 16:00:13 +0200

    add logging

--- scripts/safari12.sh
+++ scripts/safari12.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 optionols=$(grep -lnr '?\.' .next/static/**/*.js | wc -l)
- if [[ $optionols -gt 2 ]] ; then
-   echo "Oh no found $(($optionols-1)) ?."
+ if [[ $optionols -gt 3 ]] ; then
+   echo "Oh no found $(($optionols-3)) ?."
    echo $(grep -lnr '?\.' .next/static/**/*.js)
    exit 1
  else
