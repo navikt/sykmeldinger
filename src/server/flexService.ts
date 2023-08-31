62de306388e0d43f8a1a97bc66f07124fde78044
Karl J. Overå <k@rl.run>
2023-08-31 13:52:39 +0200

    add user segment

--- src/server/flexService.ts
+++ src/server/flexService.ts
@@ -7,9 +7,9 @@ import { getServerEnv } from '../utils/env'
 import { ErUtenforVentetid, ErUtenforVentetidSchema } from './api-models/ErUtenforVentetid'
 import { RequestContext } from './graphql/resolvers'
 
-const serverEnv = getServerEnv()
-
 export async function getErUtenforVentetid(sykmeldingId: string, context: RequestContext): Promise<ErUtenforVentetid> {
+    const serverEnv = getServerEnv()
+
     const childLogger = createChildLogger(context.requestId)
 
     childLogger.info(`Fetching flex er utenfor ventetid for sykmeldingId ${sykmeldingId}`)
@@ -60,7 +60,9 @@ export async function getErUtenforVentetid(sykmeldingId: string, context: Reques
     )
 }
 
-export async function feedback(feedback: unknown, context: RequestContext): Promise<boolean> {
+export async function feedback(feedback: object, context: RequestContext): Promise<boolean> {
+    const serverEnv = getServerEnv()
+
     const childLogger = createChildLogger(context.requestId)
 
     childLogger.info(`Submitting feedback to flexjar-backend`)
@@ -82,7 +84,7 @@ export async function feedback(feedback: unknown, context: RequestContext): Prom
             'Content-Type': 'application/json',
             'x-request-id': context.requestId,
         },
-        body: JSON.stringify(feedback),
+        body: JSON.stringify({ ...feedback, segment: inferSegment(context.payload.pid) }),
     })
 
     if (response.status === 401) {
@@ -99,4 +101,21 @@ export async function feedback(feedback: unknown, context: RequestContext): Prom
     throw new Error(
         `Unable to submit feedback to flexjar-backend, requestId: ${context.requestId}, status: ${response.status} ${response.statusText}`,
     )
+}
+
+export function inferSegment(pid: string): string {
+    const year = +pid.slice(4, 6)
+    const century = year > 20 ? 1900 : 2000
+    const fullYear = century + +year
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - fullYear
+
+    if (age > 69) return '69-∞'
+    else if (age > 59) return '60-69'
+    else if (age > 49) return '50-59'
+    else if (age > 39) return '40-49'
+    else if (age > 29) return '30-39'
+    else if (age > 19) return '20-29'
+    else if (age > 9) return '10-19'
+    else return '0-9'
 }
