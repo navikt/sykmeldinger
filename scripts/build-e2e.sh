2c889c51659ecffe23c2fcb0be47d0f0065ca5f7
Karl J. Overå <k@rl.run>
2023-10-05 14:20:44 +0200

    migrate arbeidsledig to playwright

--- scripts/build-e2e.sh
+++ scripts/build-e2e.sh
@@ -1,8 +1,2 @@
 cp nais/envs/.env.demo .env.production
 NEXT_PUBLIC_IS_E2E=true NEXT_PUBLIC_BASE_PATH= NEXT_PUBLIC_ASSET_PREFIX= yarn build:only
-
-if [ "$1" = "--docker" ]; then
-  echo "Building docker image"
-  docker build . -t e2e:latest
-  exit 0
-fi
