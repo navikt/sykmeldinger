bce4ae9d4358adedf8bfa82ccb70f0d1d04be92e
Karl J. Overå <k@rl.run>
2023-10-12 11:36:46 +0200

    disable animations for bekreftAvvist tests

--- e2e/bekreftAvvist.spec.tsx
+++ e2e/bekreftAvvist.spec.tsx
@@ -18,83 +18,92 @@ test.describe('Bekreft avvist sykmelding som lest', () => {
         await expect(page).toHaveNoViolations()
     })
 
-    test('should get error message when trying to submit without checking checkbox', async ({ page }) => {
-        await gotoScenario('avvist')(page)
+    test.describe('tests that toggle', () => {
+        test.beforeEach(async ({ page }) => {
+            // The axe check sometimes checks for contrast mid-animation, which fails
+            await page.emulateMedia({ reducedMotion: 'reduce' })
+        })
 
-        await page
-            .getByRole('region', { name: /Nye sykmeldinger/i })
-            .getByRole('link', { name: /Sykmelding 100%/ })
-            .click()
+        test('should get error message when trying to submit without checking checkbox', async ({ page }) => {
+            await gotoScenario('avvist')(page)
 
-        await expect(page.getByText(/Du trenger en ny sykmelding/)).toBeVisible()
-        await page.getByRole('button', { name: 'Bekreft' }).click()
+            await page
+                .getByRole('region', { name: /Nye sykmeldinger/i })
+                .getByRole('link', { name: /Sykmelding 100%/ })
+                .click()
 
-        await expect(
-            page.getByRole('checkbox', {
-                name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
-            }),
-        ).toHaveDescriptiveText('Du må bekrefte at du har lest at sykmeldingen er avvist.')
+            await expect(page.getByText(/Du trenger en ny sykmelding/)).toBeVisible()
+            await page.getByRole('button', { name: 'Bekreft' }).click()
 
-        await expect(page).toHaveNoViolations()
-    })
+            await expect(
+                page.getByRole('checkbox', {
+                    name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
+                }),
+            ).toHaveDescriptiveText('Du må bekrefte at du har lest at sykmeldingen er avvist.')
 
-    test('should remove error message after clicking checkbox', async ({ page }) => {
-        await gotoScenario('avvist')(page)
+            await expect(page).toHaveNoViolations()
+        })
 
-        await page
-            .getByRole('region', { name: /Nye sykmeldinger/i })
-            .getByRole('link', { name: /Sykmelding 100%/ })
-            .click()
+        test('should remove error message after clicking checkbox', async ({ page }) => {
+            await gotoScenario('avvist')(page)
 
-        await page.getByRole('button', { name: 'Bekreft' }).click()
-        await expect(
-            page.getByRole('checkbox', { name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist' }),
-        ).toBeVisible()
+            await page
+                .getByRole('region', { name: /Nye sykmeldinger/i })
+                .getByRole('link', { name: /Sykmelding 100%/ })
+                .click()
 
-        await expect(
-            page.getByRole('checkbox', {
-                name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
-            }),
-        ).toHaveDescriptiveText('Du må bekrefte at du har lest at sykmeldingen er avvist.')
+            await page.getByRole('button', { name: 'Bekreft' }).click()
+            await expect(
+                page.getByRole('checkbox', { name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist' }),
+            ).toBeVisible()
 
-        await expect(page).toHaveNoViolations()
+            await expect(
+                page.getByRole('checkbox', {
+                    name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
+                }),
+            ).toHaveDescriptiveText('Du må bekrefte at du har lest at sykmeldingen er avvist.')
 
-        await page
-            .getByRole('checkbox', {
-                name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
-            })
-            .click()
+            await expect(page).toHaveNoViolations()
 
-        await expect(
-            page.getByRole('checkbox', {
-                name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
-            }),
-        ).not.toHaveDescriptiveText()
+            await page
+                .getByRole('checkbox', {
+                    name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
+                })
+                .click()
 
-        await expect(page).toHaveNoViolations()
-    })
+            await expect(
+                page.getByRole('checkbox', {
+                    name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
+                }),
+            ).not.toHaveDescriptiveText()
 
-    test('should show confirmation after submitting', async ({ page }) => {
-        await gotoScenario('avvist')(page)
-        await navigateToFirstSykmelding('nye', '100%')(page)
+            await expect(page).toHaveNoViolations()
+        })
 
-        await page
-            .getByRole('checkbox', {
-                name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
-            })
-            .click()
+        test('should show confirmation after submitting', async ({ page }) => {
+            await gotoScenario('avvist')(page)
+            await navigateToFirstSykmelding('nye', '100%')(page)
 
-        await expect(page).toHaveNoViolations()
+            await page
+                .getByRole('checkbox', {
+                    name: 'Jeg bekrefter at jeg har lest at sykmeldingen er avvist',
+                })
+                .click()
 
-        await page.getByRole('button', { name: 'Bekreft' }).click()
+            await expect(page).toHaveNoViolations()
 
-        await expect(
-            page.getByText(
-                `Du bekreftet at du har lest at sykmeldingen er avvist den ${toReadableDate(subDays(new Date(), 7))}`,
-            ),
-        ).toBeVisible()
+            await page.getByRole('button', { name: 'Bekreft' }).click()
 
-        await expect(page.getByRole('button', { name: 'Ferdig' })).toBeVisible()
-        await expect(page).toHaveNoViolations()
+            await expect(
+                page.getByText(
+                    `Du bekreftet at du har lest at sykmeldingen er avvist den ${toReadableDate(
+                        subDays(new Date(), 7),
+                    )}`,
+                ),
+            ).toBeVisible()
+
+            await expect(page.getByRole('button', { name: 'Ferdig' })).toBeVisible()
+            await expect(page).toHaveNoViolations()
+        })
     })
 })
