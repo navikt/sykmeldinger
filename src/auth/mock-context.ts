d59b0a8d94d4272c1fad97bb8bdaa25791172c94
Karl J. Overå <k@rl.run>
2023-12-22 12:58:12 +0100

    chore: migrate frilanser feature tests to playwright

--- src/auth/mock-context.ts
+++ src/auth/mock-context.ts
@@ -13,10 +13,14 @@ export function handleMockContext(
 ): Promise<GetServerSidePropsResult<ServerSidePropsResult>> {
     const scenario = context.query.scenario as string | undefined
     const antallArbeidsgivere = context.query.antallArbeidsgivere as string | undefined
+    const utenforVentetid = context.query.utenforVentetid as string | undefined
+    const oppfolgingsdato = context.query.oppfolgingsdato as string | undefined
 
-    logger.info(`Setting up mock context, scenario: ${scenario}, antallArbeidsgivere: ${antallArbeidsgivere}`)
+    logger.info(
+        `Setting up mock context, scenario: ${scenario}, antallArbeidsgivere: ${antallArbeidsgivere}, utenforVentetid: ${utenforVentetid}, oppfolgingsdato: ${oppfolgingsdato}`,
+    )
 
-    if (isValidScenario(scenario) || antallArbeidsgivere) {
+    if (isValidScenario(scenario) || antallArbeidsgivere || utenforVentetid || oppfolgingsdato) {
         const newId = v4()
         context.res.setHeader('set-cookie', `next-session-id=${newId}; Path=/`)
 
@@ -28,6 +32,12 @@ export function handleMockContext(
 
         if (antallArbeidsgivere) {
             mockDb().get(newId).setAntallArbeidsgivere(+antallArbeidsgivere)
+            mockDb()
+                .get(newId)
+                .setErUtenforVentetid(utenforVentetid === 'true')
+            mockDb()
+                .get(newId)
+                .setOppfolgingsdato(oppfolgingsdato ?? '')
         }
     } else if (!context.req.cookies['next-session-id']) {
         const newId = v4()
