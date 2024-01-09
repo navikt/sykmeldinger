155183dc505f3e30843a3f806ec25fcb7dda8cb4
Natalie Uranes <natalie.uranes@nav.no>
2024-01-09 13:47:42 +0100

    add title to SladdSvg

--- src/components/SykmeldingViews/SykmeldingView/Svg/SladdSvg.tsx
+++ src/components/SykmeldingViews/SykmeldingView/Svg/SladdSvg.tsx
@@ -5,15 +5,8 @@ const SladdSvg = (): ReactElement => {
     const patternId = useId()
 
     return (
-        <svg
-            width="246"
-            height="39"
-            viewBox="0 0 246 39"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-hidden
-        >
+        <svg width="246" height="39" viewBox="0 0 246 39" fill="none" xmlns="http://www.w3.org/2000/svg" role="img">
+            <title>Sladdet bilde</title>
             <pattern id={patternId} patternUnits="userSpaceOnUse" width="4" height="4">
                 <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" style={{ stroke: '#3E3832', strokeWidth: '1' }} />
             </pattern>
