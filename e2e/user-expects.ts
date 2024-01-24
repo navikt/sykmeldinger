61036aa598275c120bb7cb58d795bd199e6fa188
Karl J. Overå <k@rl.run>
2024-01-24 13:17:06 +0100

    test: expect answers in fiskere e2e tests

--- e2e/user-expects.ts
+++ e2e/user-expects.ts
@@ -37,7 +37,7 @@ export function expectKvittering(opts: {
 export function expectDineSvar(svar: {
     stemmer?: 'Ja' | 'Nei'
     uriktige?: UriktigeOpplysningerType[]
-    arbeidssituasjon: 'Ansatt' | 'Arbeidsledig' | 'Annet'
+    arbeidssituasjon: 'Ansatt' | 'Arbeidsledig' | 'Annet' | 'Fisker'
     arbeidsgiver?: string
     narmesteleder?:
         | {
@@ -55,22 +55,29 @@ export function expectDineSvar(svar: {
               antallDager: number
           }
         | ExpectMeta.NotInDom
+    selvstendig?: {
+        egenmeldingsperioder: 'Nei' | string[] | ExpectMeta.NotInDom
+        forsikring: 'Ja' | 'Nei' | ExpectMeta.NotInDom
+    }
+    fisker?:
+        | {
+              blad: 'A' | 'B'
+              lottEllerHyre: 'Lott' | 'Hyre' | 'Begge'
+          }
+        | ExpectMeta.NotInDom
 }) {
     return async (page: Page): Promise<void> => {
         const region = page.getByRole('region', { name: /Dine svar/i })
-
-        await expect(region).toBeVisible()
-        await region.scrollIntoViewIfNeeded()
-
-        // Expand ExpansionCard
-        // await region.getByRole('button', { name: 'Vis mer' }).click()
         await region.click()
 
+        // These two questions are always asked and answered
         await expect(getInfoItem('Stemmer opplysningene?')(region)).toHaveText(new RegExp(svar.stemmer ?? 'Ja', 'i'))
         await expect(getInfoItem('Jeg er sykmeldt som')(region)).toHaveText(new RegExp(svar.arbeidssituasjon, 'i'))
+
         if (svar.arbeidsgiver) {
             await expect(getInfoItem('Velg arbeidsgiver')(region)).toHaveText(new RegExp(svar.arbeidsgiver, 'i'))
         }
+
         if (svar.narmesteleder != null && typeof svar.narmesteleder !== 'string') {
             await expect(
                 getInfoItem(
@@ -78,11 +85,13 @@ export function expectDineSvar(svar: {
                 )(region),
             ).toHaveText(new RegExp(svar.narmesteleder.svar, 'i'))
         }
+
         if (svar.narmesteleder === ExpectMeta.NotInDom) {
             await expect(
                 getInfoItem(new RegExp(`som skal følge deg opp på jobben mens du er syk?`))(region),
             ).not.toBeVisible()
         }
+
         if (svar.egenmeldingsdager) {
             if (svar.egenmeldingsdager === ExpectMeta.NotInDom) {
                 await expect(getInfoItem(new RegExp(`Brukte du egenmelding hos`))(region)).not.toBeVisible()
@@ -103,6 +112,46 @@ export function expectDineSvar(svar: {
                 )
             } else {
                 raise('Illegal state: Needs to be implemented')
+            }
+        }
+
+        if (svar.selvstendig) {
+            if (svar.selvstendig.egenmeldingsperioder === ExpectMeta.NotInDom) {
+                await expect(
+                    getInfoItem('Brukte du egenmelding eller noen annen sykmelding før denne datoen?')(page),
+                ).not.toBeVisible()
+            } else if (svar.selvstendig.egenmeldingsperioder === 'Nei') {
+                await expect(
+                    getInfoItem('Brukte du egenmelding eller noen annen sykmelding før denne datoen?')(page),
+                ).toHaveText(/Nei/)
+            } else {
+                await expect(
+                    getInfoItem('Brukte du egenmelding eller noen annen sykmelding før denne datoen?')(page),
+                ).toHaveText(/Ja/)
+                await expect(getInfoItem(/Hvilke dager var du borte fra jobb før/)(page)).toHaveText(
+                    new RegExp(svar.selvstendig.egenmeldingsperioder.join(''), 'i'),
+                )
+            }
+
+            if (svar.selvstendig.forsikring === ExpectMeta.NotInDom) {
+                await expect(
+                    getInfoItem('Har du forsikring som gjelder for de første 16 dagene av sykefraværet?')(page),
+                ).not.toBeVisible()
+            } else {
+                await expect(
+                    getInfoItem('Har du forsikring som gjelder for de første 16 dagene av sykefraværet?')(page),
+                ).toHaveText(new RegExp(svar.selvstendig.forsikring, 'i'))
+            }
+        }
+
+        if (svar.fisker) {
+            if (svar.fisker === ExpectMeta.NotInDom) {
+                await expect(getInfoItem('Velg blad')(page)).not.toBeVisible()
+            } else {
+                await expect(getInfoItem('Velg blad')(page)).toHaveText(new RegExp(`Blad ${svar.fisker.blad}`, 'i'))
+                await expect(getInfoItem('Mottar du lott eller er du på hyre?')(page)).toHaveText(
+                    new RegExp(svar.fisker.lottEllerHyre, 'i'),
+                )
             }
         }
     }
