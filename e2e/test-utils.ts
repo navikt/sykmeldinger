b852867c8b044a17a162e956036d9e96db774425
Karl J. Overå <k@rl.run>
2024-01-24 15:58:47 +0100

    test: more assertions on answers for frilanser, jordbruker, permittert etc

--- e2e/test-utils.ts
+++ e2e/test-utils.ts
@@ -11,3 +11,13 @@ export function getRadioInGroup(page: Page) {
 export function getCheckboxInGroup(page: Page) {
     return (group: ByRoleOptions, radio: ByRoleOptions) => page.getByRole('group', group).getByRole('checkbox', radio)
 }
+
+type CurriedAction = (page: Page) => Promise<void>
+
+export function userInteractionsGroup(...actions: CurriedAction[]) {
+    return async (page: Page): Promise<void> => {
+        for (const action of actions) {
+            await action(page)
+        }
+    }
+}
