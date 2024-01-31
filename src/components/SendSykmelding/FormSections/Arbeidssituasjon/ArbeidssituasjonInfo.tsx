1765be5b6fa48105d139a90a62a7e9020539a402
Natalie Uranes <natalie.uranes@nav.no>
2024-01-31 10:39:10 +0100

    chore: remove unused info link

--- src/components/SendSykmelding/FormSections/Arbeidssituasjon/ArbeidssituasjonInfo.tsx
+++ src/components/SendSykmelding/FormSections/Arbeidssituasjon/ArbeidssituasjonInfo.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react'
-import { BodyLong, BodyShort, Link, ReadMore } from '@navikt/ds-react'
+import { BodyShort, ReadMore } from '@navikt/ds-react'
 
 export function ArbeidssituasjonInfo(): ReactElement {
     return (
@@ -10,23 +10,6 @@ export function ArbeidssituasjonInfo(): ReactElement {
             <ReadMore header="Har du flere arbeidsforhold?">
                 Du trenger én sykmelding for hvert arbeidsforhold du er sykmeldt fra. Kontakt den som har sykmeldt deg.
             </ReadMore>
-        </div>
-    )
-}
-
-export function ArbeidssituasjonStatusInfo(): ReactElement {
-    //TODO: fix broken url or remove link
-    return (
-        <div className="mb-1 mt-4">
-            <BodyLong>
-                Sjekk om du er{' '}
-                <Link
-                    href="https://www.nav.no/no/person/innhold-til-person-forside/nyttig-a-vite/er-jeg-selvstendig-naeringsdrivende-frilanser-eller-arbeidstaker"
-                    target="_blank"
-                >
-                    selvstendig næringsdrivende, frilanser eller ansatt
-                </Link>
-            </BodyLong>
         </div>
     )
 }
