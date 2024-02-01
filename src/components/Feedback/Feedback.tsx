e34c8bafb9b0e2b902dd64cdc47d32ed3919a877
Karl J. Overå <k@rl.run>
2024-02-01 15:25:49 +0100

    fix: don't submit flexjar on enter, only ctrl+enter

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -98,7 +98,7 @@ function Feedback({ feedbackId, metadata }: Props): ReactElement {
                                     error={errorMessage}
                                     label={responseTypeToLabelMap[activeResponseType]}
                                     onKeyDown={async (e) => {
-                                        if (e.key === 'Enter' && !e.shiftKey) {
+                                        if (e.key === 'Enter' && e.ctrlKey) {
                                             e.preventDefault()
                                             await handleSend()
                                         }
