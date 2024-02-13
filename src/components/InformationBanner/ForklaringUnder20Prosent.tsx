0d4c2cffe214ba33c41dec40b0d0569631f174a7
Karl J. Overå <k@rl.run>
2024-02-13 08:44:21 +0100

    feat: clarify text for sykmelding below 20%

--- src/components/InformationBanner/ForklaringUnder20Prosent.tsx
+++ src/components/InformationBanner/ForklaringUnder20Prosent.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { BodyShort, BodyLong, GuidePanel } from '@navikt/ds-react'
+import { BodyLong, GuidePanel } from '@navikt/ds-react'
 
 type Props = {
     grad: number
@@ -8,16 +8,20 @@ type Props = {
 function ForklaringUnder20Prosent({ grad }: Props): ReactElement {
     return (
         <GuidePanel poster>
-            <BodyShort spacing>Under ser du opplysningene vi har fått fra behandleren din.</BodyShort>
+            <BodyLong spacing>Under ser du opplysningene vi har fått fra behandleren din.</BodyLong>
             <BodyLong spacing>
                 Denne sykmeldingen viser at du er {grad} prosent sykmeldt. Hvis du er under 20 prosent sykmeldt, har du
-                ikke rett til sykepenger. Om du er sykmeldt fra flere arbeidsforhold, kan du i noen tilfeller ha rett til
-                sykepenger likevel.
+                ikke rett til sykepenger. Om du er sykmeldt fra flere arbeidsforhold, kan du i noen tilfeller ha rett
+                til sykepenger likevel.
+            </BodyLong>
+            <BodyLong spacing>
+                Hvis sykmeldingsgraden er feil må du avbryte denne sykmeldingen og kontakte den som har sykmeldt deg for
+                å få en ny.
             </BodyLong>
             <BodyLong>
-                NAV kan ikke hindre deg i å sende inn søknad om sykepenger, selv om det er riktig at du har vært
-                sykmeldt under 20 prosent. Søknad om sykepenger blir tilgengelig etter at sykmeldingsperioden er over.
-                Hvis du ikke skal søke om sykepenger, kan du avbryte søknaden.
+                Hvis opplysningene i sykmeldingen stemmer, sender du sykmeldingen, nederst på siden. Når
+                sykmeldingsperioden er over må du sende søknad om sykepenger for å få vurdert om du har rett til
+                sykepenger.
             </BodyLong>
         </GuidePanel>
     )
