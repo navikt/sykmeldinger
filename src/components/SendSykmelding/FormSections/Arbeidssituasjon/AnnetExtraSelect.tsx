489207283e3bf6032c61cc6ed4cff130d5949750
Karl J. Overå <k@rl.run>
2024-03-21 12:55:22 +0100

    feat: tell annet → lærling that they are in fact, normal ansatte

--- src/components/SendSykmelding/FormSections/Arbeidssituasjon/AnnetExtraSelect.tsx
+++ src/components/SendSykmelding/FormSections/Arbeidssituasjon/AnnetExtraSelect.tsx
@@ -6,7 +6,7 @@ import { useController, useFormContext } from 'react-hook-form'
 import { FormValues } from '../../SendSykmeldingForm'
 import { logAmplitudeEvent } from '../../../../amplitude/amplitude'
 
-const initialOptions = ['Student', 'Konkurs', 'Flere arbeidsgivere']
+const initialOptions = ['Student', 'Konkurs', 'Lærling', 'Flere arbeidsgivere']
 
 function AnnetExtraSelect(): ReactElement {
     const { register } = useFormContext<FormValues>()
@@ -15,39 +15,59 @@ function AnnetExtraSelect(): ReactElement {
     })
 
     return (
-        <div className="mt-4 max-w-md">
-            <Select
-                onChange={(event) => {
-                    field.onChange(event.currentTarget.value)
+        <>
+            <div className="mt-4 max-w-md">
+                <Select
+                    onChange={(event) => {
+                        field.onChange(event.currentTarget.value)
 
-                    logAmplitudeEvent({
-                        eventName: 'skjema spørsmål besvart',
-                        data: {
-                            skjemanavn: 'åpen sykmelding',
-                            spørsmål: 'Hvilken situasjon er du i som gjorde at du valgte annet? (draft)',
-                            svar: event.currentTarget.value,
-                        },
-                    })
-                }}
-                label="Hvilken situasjon er du i som gjorde at du valgte annet?"
-            >
-                <option value="" disabled>
-                    Velg situasjon
-                </option>
-                {R.pipe(
-                    initialOptions,
-                    R.map((option) => (
-                        <option key={option} value={option}>
-                            {option}
-                        </option>
-                    )),
+                        logAmplitudeEvent({
+                            eventName: 'skjema spørsmål besvart',
+                            data: {
+                                skjemanavn: 'åpen sykmelding',
+                                spørsmål: 'Hvilken situasjon er du i som gjorde at du valgte annet? (draft)',
+                                svar: event.currentTarget.value,
+                            },
+                        })
+                    }}
+                    label="Hvilken situasjon er du i som gjorde at du valgte annet?"
+                >
+                    <option value="" disabled selected>
+                        Velg situasjon
+                    </option>
+                    {R.pipe(
+                        initialOptions,
+                        R.map((option) => (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        )),
+                    )}
+                    <option value="annet-annet">Ingen av disse, skriv inn selv</option>
+                </Select>
+                {field.value && field.value === 'annet-annet' && (
+                    <>
+                        <TextField
+                            {...register('extra.annetSituasjonTekst')}
+                            label="Beskriv din situasjon selv"
+                            className="mt-4"
+                            maxLength={25}
+                        />
+                        <BodyShort spacing size="small" className="mt-1 italic">
+                            1-2 ord, ikke skriv navn eller lignende personopplysninger
+                        </BodyShort>
+                    </>
                 )}
-                <option value="annet-annet">Ingen av disse, skriv inn selv</option>
-            </Select>
-            {field.value && field.value === 'annet-annet' && (
-                <TextField {...register('extra.annetSituasjonTekst')} label="Beskrivelse" className="mt-4" />
-            )}
-            {field.value && field.value === 'Flere arbeidsgivere' && (
+            </div>
+            <TryToHelpWarnings value={field.value} />
+        </>
+    )
+}
+
+function TryToHelpWarnings({ value }: { value: string | null }): ReactElement | null {
+    switch (value) {
+        case 'Flere arbeidsgivere':
+            return (
                 <Alert variant="warning" className="mt-4">
                     <BodyShort spacing>
                         Hvis du har flere arbeidsgivere, må du sende en sykmelding for hver arbeidsgiver.
@@ -61,9 +81,17 @@ function AnnetExtraSelect(): ReactElement {
                         og be om dette.
                     </BodyShort>
                 </Alert>
-            )}
-        </div>
-    )
+            )
+        case 'Lærling':
+            return (
+                <Alert variant="warning" className="mt-4">
+                    <BodyShort spacing>Som lærling er du ansatt på samme måte som en arbeidstaker</BodyShort>
+                    <BodyShort className="font-bold">{`Du må endre valget ditt over fra "Annet" til "Arbeidstaker".`}</BodyShort>
+                </Alert>
+            )
+        default:
+            return null
+    }
 }
 
 export default AnnetExtraSelect
