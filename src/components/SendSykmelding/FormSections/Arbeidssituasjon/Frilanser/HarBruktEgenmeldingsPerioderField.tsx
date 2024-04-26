1d2697413ff81a9a2d87b67afad251fd4a9d0da2
Natalie Uranes <natalie.uranes@nav.no>
2024-04-26 12:23:41 +0200

    fix: fravaer for sykmelding info text

--- src/components/SendSykmelding/FormSections/Arbeidssituasjon/Frilanser/HarBruktEgenmeldingsPerioderField.tsx
+++ src/components/SendSykmelding/FormSections/Arbeidssituasjon/Frilanser/HarBruktEgenmeldingsPerioderField.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react'
-import { BodyShort, Label, ReadMore } from '@navikt/ds-react'
+import { BodyShort, ReadMore } from '@navikt/ds-react'
 
 import { sporsmal } from '../../../../../utils/sporsmal'
 import YesNoField from '../../../../FormComponents/YesNoField/YesNoField'
@@ -37,33 +37,12 @@ function HarBruktEgenmeldingsPerioderField({ oppfolgingsdato }: Props): ReactEle
 }
 
 function HarBruktEgenmeldingReadMore(): ReactElement {
-    const fravaerListId = 'fravaer-list'
     return (
         <ReadMore header="Hva betyr dette?">
             <BodyShort spacing>Vi trenger denne informasjonen for å vite hvem som skal utbetale hva.</BodyShort>
             <BodyShort spacing>
                 Siden vi ikke får tak i informasjonen automatisk, må vi få disse opplysningene fra deg.
             </BodyShort>
-            <Label id={fravaerListId} spacing>
-                Med fravær mener vi:
-            </Label>
-            <ul aria-labelledby={fravaerListId}>
-                <li>
-                    <BodyShort>- egenmelding</BodyShort>
-                </li>
-                <li>
-                    <BodyShort>- papirsykmelding</BodyShort>
-                </li>
-                <li>
-                    <BodyShort>- ferie</BodyShort>
-                </li>
-                <li>
-                    <BodyShort>- permisjon</BodyShort>
-                </li>
-                <li>
-                    <BodyShort>- sykmelding fra et annet arbeidsforhold i samme bedrift</BodyShort>
-                </li>
-            </ul>
         </ReadMore>
     )
 }
--- src/utils/sporsmal.ts
+++ src/utils/sporsmal.ts
@@ -12,7 +12,7 @@ export const sporsmal = {
     riktigNarmesteLeder: (narmesteLederNavn: string) =>
         `Er det ${narmesteLederNavn} som skal følge deg opp på jobben mens du er syk?`,
     harBruktEgenmelding: (oppfolgingsdato: string, createDate = () => toReadableDate(oppfolgingsdato)) =>
-        `Vi har registrert at du ble syk ${createDate()}. Brukte du egenmelding eller noen annen sykmelding før denne datoen?`,
+        `Vi har registrert at du ble syk ${createDate()}. Brukte du egenmelding eller papirsykmelding før denne datoen?`,
     egenmeldingsperioder: (oppfolgingsdato: string) =>
         `Hvilke dager var du borte fra jobb før ${toReadableDate(oppfolgingsdato)}?`,
     harForsikring: 'Har du forsikring som gjelder for de første 16 dagene av sykefraværet?',
