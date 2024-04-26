c4cd8140193a79d47769a7b2f9581165a8d01b7b
Natalie Uranes <natalie.uranes@nav.no>
2024-04-26 16:58:06 +0200

    chore: remove tidligere arbeidsgivere list

--- src/server/api-models/sykmelding/ArbeidsgiverSykmelding.ts
+++ src/server/api-models/sykmelding/ArbeidsgiverSykmelding.ts
@@ -4,10 +4,3 @@ export type ArbeidsgiverSykmelding = z.infer<typeof ArbeidsgiverSykmeldingSchema
 export const ArbeidsgiverSykmeldingSchema = z.object({
     navn: z.string().nullable(),
 })
-
-export type TidligereArbeidsgiverList = z.infer<typeof TidligereArbeidsgiverListSchema>
-export const TidligereArbeidsgiverListSchema = z.object({
-    orgNavn: z.string(),
-    orgnummer: z.string(),
-    sykmeldingsId: z.string(),
-})
