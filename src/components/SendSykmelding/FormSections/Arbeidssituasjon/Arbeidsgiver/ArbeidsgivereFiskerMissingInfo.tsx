fcb1a4fc878f917cc30146ec3882696061b6753a
Natalie Uranes <natalie.uranes@nav.no>
2024-08-06 14:56:07 +0200

    feat: add custom missing ag error

--- src/components/SendSykmelding/FormSections/Arbeidssituasjon/Arbeidsgiver/ArbeidsgivereFiskerMissingInfo.tsx
+++ src/components/SendSykmelding/FormSections/Arbeidssituasjon/Arbeidsgiver/ArbeidsgivereFiskerMissingInfo.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react'
-import { Alert, BodyLong } from '@navikt/ds-react'
+import { Alert, BodyLong, Heading } from '@navikt/ds-react'
 
 import { Blad, LottOgHyre } from 'queries'
 
@@ -23,10 +23,13 @@ function ArbeidsgivereFiskerMissingInfo({ metadata }: Props): ReactElement {
 
     return (
         <Alert className="mt-4" variant="warning">
+            <Heading spacing size="small" level="2">
+                Vi finner ingen arbeidsforhold registrert på deg
+            </Heading>
             <BodyLong spacing>
-                Vi klarer ikke å finne noen arbeidsforhold registrert på deg. Dette vil gjelde en del fiskere på hyre,
-                da arbeidsgiver ikke har plikt til å melde inn arbeidsforholdet. Hvis du er usikker på om det er riktig
-                i ditt tilfelle, kan du ta kontakt med arbeidsgiveren din.
+                Dette vil gjelde en del fiskere på hyre, da arbeidsgiver ikke har plikt til å melde inn
+                arbeidsforholdet. Hvis du er usikker på om det er riktig i ditt tilfelle, kan du ta kontakt med
+                arbeidsgiveren din.
             </BodyLong>
             <BodyLong>
                 Hvis det stemmer at arbeidsforholdet ditt ikke skal registreres, kan du sende inn sykmeldingen til NAV
