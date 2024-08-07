a9cd69646d06d91d7422c2a2f9e40d1d6436f4ff
Karl J. Overå <k@rl.run>
2024-08-07 16:27:01 +0200

    chore: tweak text

--- src/components/SendSykmelding/FormSections/Arbeidssituasjon/ArbeidssituasjonInfo.tsx
+++ src/components/SendSykmelding/FormSections/Arbeidssituasjon/ArbeidssituasjonInfo.tsx
@@ -4,8 +4,8 @@ import { BodyShort } from '@navikt/ds-react'
 export function ArbeidssituasjonInfo(): ReactElement {
     return (
         <BodyShort className="mt-4 -mb-4">
-            Hvis du er sykmeldt fra flere jobber, må du be legen din om én sykmelding for hver av dem. Dette gjelder
-            også hvis du har flere stillinger hos samme arbeidsgiver.
+            Du trenger én sykmelding for hver arbeidssituasjon du er sykmeldt fra. Har du flere jobber, må du be legen
+            din om én sykmelding for hver av dem.
         </BodyShort>
     )
 }
