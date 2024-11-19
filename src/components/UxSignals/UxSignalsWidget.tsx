634117b7c77bba7a49838508a4ebcf7b1f5203f4
Natalie Uranes <natalie.uranes@nav.no>
2024-11-19 15:03:07 +0100

    chore: add ux signals for naeringsdrivende

--- src/components/UxSignals/UxSignalsWidget.tsx
+++ src/components/UxSignals/UxSignalsWidget.tsx
@@ -4,19 +4,15 @@ import { ReactElement } from 'react'
 import { isLocalOrDemo } from '../../utils/env'
 import styles from '../../components/UxSignals/UxSignalsWidget.module.css'
 
-const DISABLE_UX_SIGNALS = true
+const DISABLE_UX_SIGNALS = false
 
 function UxSignalsWidget(): ReactElement | null {
     if (isLocalOrDemo || DISABLE_UX_SIGNALS) return null
 
     return (
         <>
-            <Script
-                type="module"
-                strategy="lazyOnload"
-                src="https://uxsignals-frontend.uxsignals.app.iterate.no/embed.js"
-            />
-            <div data-uxsignals-embed="panel-etd0zg7795" className={styles.uxSignalsContainer} />
+            <div data-uxsignals-embed="panel-gtnepi3ujl" className={styles.uxSignalsContainer}></div>
+            <Script type="module" strategy="lazyOnload" src="https://widget.uxsignals.com/embed.js" />
         </>
     )
 }
