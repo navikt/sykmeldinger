031927d5398663f4afcdfdaa3343ba978b03c214
Natalie Uranes <natalie.uranes@nav.no>
2024-11-22 11:21:31 +0100

    chore: disable ux signals

--- src/components/UxSignals/UxSignalsWidget.tsx
+++ src/components/UxSignals/UxSignalsWidget.tsx
@@ -4,7 +4,7 @@ import { ReactElement } from 'react'
 import { isLocalOrDemo } from '../../utils/env'
 import styles from '../../components/UxSignals/UxSignalsWidget.module.css'
 
-const DISABLE_UX_SIGNALS = false
+const DISABLE_UX_SIGNALS = true
 
 function UxSignalsWidget(): ReactElement | null {
     if (isLocalOrDemo || DISABLE_UX_SIGNALS) return null
