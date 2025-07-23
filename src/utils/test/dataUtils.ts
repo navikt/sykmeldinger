dc78e634aa2fce6fb9f153cefbcfafcda58c7669
Andreas Sagen Aspaas <danduras@gmail.com>
2025-07-23 21:30:04 +0200

    Revert "chore: update deps"
    This reverts commit ecc35ec518b68f51808c1528f6b294e9545ff060.

--- src/utils/test/dataUtils.ts
+++ src/utils/test/dataUtils.ts
@@ -1,5 +1,5 @@
 import { MockedResponse } from '@apollo/client/testing'
-import {Cache, TypedDocumentNode, Unmasked} from '@apollo/client'
+import { Cache, TypedDocumentNode } from '@apollo/client'
 import { FetchResult } from '@apollo/client/link/core'
 import { ResultFunction } from '@apollo/client/testing/core/mocking/mockLink'
 
@@ -272,7 +272,7 @@ export function createInitialQuery<Query, Variables>(
 ): Cache.WriteQueryOptions<Query, Variables> {
     return {
         query: typedDocumentNode,
-        data: data as Unmasked<Query>,
+        data,
         variables,
     }
 }
@@ -283,8 +283,8 @@ export function createMock<Query, Variables extends Record<string, unknown>>(moc
     error?: Error
     delay?: number
     newData?: ResultFunction<FetchResult<Query>, Record<string, unknown>>
-}): MockedResponse<Unmasked<Query>> {
-    return mockedResponse as MockedResponse<Unmasked<Query>>
+}): MockedResponse<Query> {
+    return mockedResponse
 }
 
 export function createEgenmeldingsdagerSporsmal(dates: string[]): SykmeldingStatusFragment['sporsmalOgSvarListe'][0] {
